5fe156364684ea394df74d4305e8eab3dd37e75a
Wildan Zulfikar <wzulfikar@users.noreply.github.com>
2025-04-04 15:37:06 +0300

    Update 20210219002017_migrate_es_indices_document_type.rb

--- db/migrate/20210219002017_migrate_es_indices_document_type.rb
+++ db/migrate/20210219002017_migrate_es_indices_document_type.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class MigrateEsIndicesDocumentType < ActiveRecord::Migration[6.1]
-  # Original migration: github.com/antiwork/gumroad/blob/main/db/migrate/20210219002017_migrate_es_indices_document_type.rb
+  # Original migration: https://github.com/antiwork/gumroad/blob/main/db/migrate/20210219002017_migrate_es_indices_document_type.rb
 
   def up
     return if Rails.env.production?
