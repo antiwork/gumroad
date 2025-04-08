3d6539079532e66041f0fb2bd2826d7bad10ba90
Razvan Marescu <razvan@marescu.net>
2025-04-07 19:12:53 -0700

    wip

--- app/models/refund_policy.rb
+++ app/models/refund_policy.rb
@@ -5,12 +5,24 @@ class RefundPolicy < ApplicationRecord
 
   has_paper_trail
 
+  ALLOWED_REFUND_PERIODS_IN_DAYS = {
+    0 => "No refunds allowed",
+    7 => "7-day money back guarantee",
+    14 => "14-day money back guarantee",
+    30 => "30-day money back guarantee",
+    183 => "6-month money back guarantee",
+  }.freeze
+  DEFAULT_REFUND_PERIOD_IN_DAYS = 30
+
   belongs_to :seller, class_name: "User"
 
   stripped_fields :title, :fine_print, transform: -> { ActionController::Base.helpers.strip_tags(_1) }
 
   validates_presence_of :seller
   validates :fine_print, length: { maximum: 3_000 }
+  attribute :max_refund_period_in_days, :integer, default: DEFAULT_REFUND_PERIOD_IN_DAYS
+
+  validates :max_refund_period_in_days, inclusion: { in: ALLOWED_REFUND_PERIODS_IN_DAYS }
 
   def as_json(*)
     {
