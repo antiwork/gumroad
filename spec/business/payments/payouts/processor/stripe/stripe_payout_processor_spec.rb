29a30488aeba6496859827a2d9f811dc785b1771
Seth Thompson <s3th.thompson@gmail.com>
2025-04-11 11:25:39 -0400

    update specs after move

--- spec/business/payments/payouts/processor/stripe/stripe_payout_processor_spec.rb
+++ spec/business/payments/payouts/processor/stripe/stripe_payout_processor_spec.rb
@@ -3427,4 +3427,35 @@ describe StripePayoutProcessor, :vcr do
       end
     end
   end
+
+  describe ".instantly_payable_balances" do
+    it "returns maximum unpaid balances whose sum is less than instantly payable amount available on Stripe" do
+      user = create(:user)
+      merchant_account = create(:merchant_account_stripe, user:)
+
+      bal1 = create(:balance, user:, merchant_account:, date: Date.current - 3.days, amount_cents: 500_00, holding_amount_cents: 500_00)
+      bal2 = create(:balance, user:, merchant_account:, date: Date.current - 2.days, amount_cents: 400_00, holding_amount_cents: 400_00)
+      bal3 = create(:balance, user:, merchant_account:, date: Date.current - 1.days, amount_cents: 300_00, holding_amount_cents: 300_00)
+      bal4 = create(:balance, user:, merchant_account:, date: Date.current, amount_cents: 200_00, holding_amount_cents: 200_00)
+      bal5 = create(:balance, user:, date: Date.current - 3.days, amount_cents: 500_00, holding_amount_cents: 500_00)
+      bal6 = create(:balance, user:, date: Date.current - 2.days, amount_cents: 500_00, holding_amount_cents: 500_00)
+      bal7 = create(:balance, user:, date: Date.current - 1.days, amount_cents: 500_00, holding_amount_cents: 500_00)
+      bal8 = create(:balance, user:, date: Date.current, amount_cents: 500_00, holding_amount_cents: 500_00)
+
+      allow(StripePayoutProcessor).to receive(:instantly_payable_amount_cents_on_stripe).with(user).and_return(1400_00)
+      expect(StripePayoutProcessor.instantly_payable_balances(user)).to match_array([bal1, bal2, bal3, bal4, bal5, bal6, bal7, bal8])
+
+      allow(StripePayoutProcessor).to receive(:instantly_payable_amount_cents_on_stripe).with(user).and_return(1000_00)
+      expect(StripePayoutProcessor.instantly_payable_balances(user)).to match_array([bal1, bal2, bal5, bal6])
+
+      allow(StripePayoutProcessor).to receive(:instantly_payable_amount_cents_on_stripe).with(user).and_return(1200_00)
+      expect(StripePayoutProcessor.instantly_payable_balances(user)).to match_array([bal1, bal2, bal3, bal5,  bal6, bal7])
+
+      allow(StripePayoutProcessor).to receive(:instantly_payable_amount_cents_on_stripe).with(user).and_return(600_00)
+      expect(StripePayoutProcessor.instantly_payable_balances(user)).to match_array([bal1, bal5])
+
+      allow(StripePayoutProcessor).to receive(:instantly_payable_amount_cents_on_stripe).with(user).and_return(1500_00)
+      expect(StripePayoutProcessor.instantly_payable_balances(user)).to match_array([bal1, bal2, bal3, bal4, bal5, bal6, bal7, bal8])
+    end
+  end
 end
