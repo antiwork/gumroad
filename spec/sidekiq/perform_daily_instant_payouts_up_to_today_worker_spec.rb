825a4e33c690926bbf054564e6ae7c743372fedb
Seth Thompson <s3th.thompson@gmail.com>
2025-04-14 03:47:17 -0400

    rename spec

--- spec/sidekiq/perform_daily_instant_payouts_up_to_today_worker_spec.rb
+++ spec/sidekiq/perform_daily_instant_payouts_up_to_today_worker_spec.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-describe PerformDailyInstantPayoutsUpToYesterdayWorker do
+describe PerformDailyInstantPayoutsUpToTodayWorker do
   describe "perform" do
-    let(:payout_period_end_date) { Date.yesterday }
+    let(:payout_period_end_date) { Date.current }
 
     it "calls 'create_daily_instant_payments_for_balances_up_to_date' on 'Payouts' which will do all the work" do
       expect(Payouts).to receive(:create_daily_instant_payments_for_balances_up_to_date).with(payout_period_end_date)
