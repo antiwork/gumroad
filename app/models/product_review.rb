f4b1db6840f082f0d2581de88a5c59c4e4c9cc15
Kyle <github@kylezh.com>
2025-04-17 12:49:50 -0400

    Add video thumbnail to review_submitted emails

--- app/models/product_review.rb
+++ app/models/product_review.rb
@@ -15,6 +15,7 @@ class ProductReview < ApplicationRecord
   has_many :videos, dependent: :destroy, class_name: "ProductReviewVideo"
   has_many :alive_videos, -> { alive }, class_name: "ProductReviewVideo"
   has_one :approved_video, -> { alive.approved }, class_name: "ProductReviewVideo"
+  has_one :pending_video, -> { alive.pending_review }, class_name: "ProductReviewVideo"
 
   scope :visible_on_product_page,
         -> {
