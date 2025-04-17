6fb4b0a2664dd6fd14518a1ae8c3affa3ffcbf45
Kyle <github@kylezh.com>
2025-04-17 00:15:06 -0400

    fix reviewvideoplayer

--- app/javascript/components/ReviewVideoPlayer.tsx
+++ app/javascript/components/ReviewVideoPlayer.tsx
@@ -1,52 +1,65 @@
-import React from "react";
+import cx from "classnames";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 
+import { getStreamingUrls } from "$app/data/product_reviews";
 import { createJWPlayer } from "$app/utils/jwPlayer";
 
+import { LoadingSpinner } from "$app/components/LoadingSpinner";
 import { PlayVideoIcon } from "$app/components/PlayVideoIcon";
 
-export const ReviewVideoPlayer = ({ sources, thumbnail }: { sources: string[]; thumbnail?: string }) => {
-  const uid = React.useId();
+function usePlayer(videoId: string, uid: string) {
+  const [loading, setLoading] = useState(false);
+  const [showPlayer, setShowPlayer] = useState(false);
+  const playerRef = useRef<jwplayer.JWPlayer | null>(null);
 
-  const player = React.useRef<jwplayer.JWPlayer | null>(null);
-  const [showPlayer, setShowPlayer] = React.useState(false);
+  const onPlay = useCallback(async () => {
+    setLoading(true);
 
-  React.useEffect(() => {
-    if (!showPlayer) return;
+    const { streaming_urls } = await getStreamingUrls(videoId);
 
-    void createJWPlayer(`${uid}-video`, {
-      playlist: [
-        {
-          sources: sources.map((source) => ({ file: source })),
-        },
-      ],
-    }).then((jwPlayer) => {
-      player.current = jwPlayer;
-      jwPlayer
-        .on("ready", () => {
-          if (player.current?.getState() === "playing") {
-            player.current.play();
-          }
-        })
-        .on("complete", () => {
-          setShowPlayer(false);
-          player.current = null;
-        });
+    playerRef.current = await createJWPlayer(`${uid}-video`, {
+      playlist: [{ sources: streaming_urls.map((file) => ({ file })) }],
     });
-  }, [showPlayer]);
+    playerRef.current.on("ready", () => {
+      setShowPlayer(true);
+      playerRef.current?.play();
+    });
+  }, [videoId, uid]);
 
-  const playerElement = () => <div id={`${uid}-video`}></div>;
-  const thumbnailElement = () => (
-    <figure className="relative aspect-video w-full">
-      <img src={thumbnail} className="absolute h-full w-full rounded-t bg-black object-cover" />
-      <button
-        className="link absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
-        onClick={() => setShowPlayer(true)}
-        aria-label="Watch"
-      >
-        <PlayVideoIcon />
-      </button>
-    </figure>
+  useEffect(
+    () => () => {
+      if (playerRef.current) {
+        playerRef.current.remove();
+        playerRef.current = null;
+      }
+    },
+    [],
   );
 
-  return <div className="w-full overflow-hidden rounded">{showPlayer ? playerElement() : thumbnailElement()}</div>;
+  return { loading, showPlayer, onPlay, playerId: `${uid}-video` };
+}
+
+export const ReviewVideoPlayer = ({ id, thumbnail }: { id: string; thumbnail?: string | null }) => {
+  const uid = React.useId();
+
+  const { loading, showPlayer, onPlay, playerId } = usePlayer(id, uid);
+
+  return (
+    <div className="relative aspect-video w-full overflow-hidden rounded bg-black">
+      <div id={playerId} className={cx({ hidden: !showPlayer })}></div>
+      <figure className={cx("relative aspect-video w-full", { hidden: showPlayer })}>
+        {thumbnail ? (
+          <img src={thumbnail} loading="lazy" className="absolute h-full w-full rounded-t bg-black object-cover" />
+        ) : null}
+        <button
+          className="link absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
+          onClick={() => void onPlay()}
+          aria-label="Watch"
+          disabled={loading}
+        >
+          {loading ? <LoadingSpinner width="3em" /> : <PlayVideoIcon />}
+        </button>
+      </figure>
+    </div>
+  );
 };
