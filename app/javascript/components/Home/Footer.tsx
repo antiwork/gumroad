e3226b9ac7ddb0ea95ba3ca9cd0af8009db44224
Maddie Hill <maddie@gumroad.com>
2025-05-06 09:03:32 -0400

    Move taxes page content to pricing page

--- app/javascript/components/Home/Footer.tsx
+++ app/javascript/components/Home/Footer.tsx
@@ -27,7 +27,7 @@ export const Footer = () => (
     <div className="flex w-full max-w-3xl flex-col gap-16">
       <div className="flex flex-1 gap-16">
         <div className="flex flex-1 flex-col gap-3">
-          <a href="/taxes" className="no-underline hover:text-pink">
+          <a href="/pricing" className="no-underline hover:text-pink">
             Taxes
           </a>
           <a href="/terms" className="no-underline hover:text-pink">
