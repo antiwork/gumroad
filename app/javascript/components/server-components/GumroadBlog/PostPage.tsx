2c067727ebc1248f5568204e4950de0815295367
Kyle <github@kylezh.com>
2025-05-29 05:21:34 -0400

    Update styling for Post page

--- app/javascript/components/server-components/GumroadBlog/PostPage.tsx
+++ app/javascript/components/server-components/GumroadBlog/PostPage.tsx
@@ -1,15 +1,26 @@
 import { EditorContent } from "@tiptap/react";
+import cx from "classnames";
 import * as React from "react";
 import { createCast } from "ts-safe-cast";
 
 import { incrementPostViews } from "$app/data/view_event";
 import { register } from "$app/utils/serverComponentUtil";
 
+import { Icon } from "$app/components/Icons";
 import { LoadingSpinner } from "$app/components/LoadingSpinner";
 import { useRichTextEditor } from "$app/components/RichTextEditor";
 import { formatPostDate } from "$app/components/server-components/Profile/PostPage";
 import { useUserAgentInfo } from "$app/components/UserAgent";
 import { useRunOnce } from "$app/components/useRunOnce";
+
+const BackToBlog = ({ className }: { className?: string }) => (
+  <div className={cx("scoped-tailwind-preflight", className)}>
+    <a href="/blog" className="text-pink-600 hover:text-pink-800 mt-4 flex items-center font-medium">
+      <Icon name="arrow-left" className="mr-1.5" style={{ width: 18, height: 18 }} />
+      Back to Blog
+    </a>
+  </div>
+);
 
 const PostPage = ({
   external_id,
@@ -37,32 +48,36 @@ const PostPage = ({
   const publishedAtFormatted = formatPostDate(published_at, userAgentInfo.locale);
 
   return (
-    <main>
-      <header>
-        <h1>{subject}</h1>
-        <time>{publishedAtFormatted}</time>
-      </header>
-      <article style={{ display: "grid", gap: "var(--spacer-6)" }}>
-        {pageLoaded ? null : <LoadingSpinner width="2em" />}
-        <EditorContent className="rich-text" editor={editor} />
+    <div className="container mx-auto px-8 py-16 sm:px-6 md:py-24 lg:px-8">
+      <div className="mx-auto max-w-3xl">
+        <BackToBlog className="mb-6" />
+        <header>
+          <h1 className="mb-4">{subject}</h1>
+          <time className="text-dark-gray">{publishedAtFormatted}</time>
+        </header>
+        <div className="mx-auto grid max-w-3xl gap-6 border-b py-12 text-xl">
+          {pageLoaded ? null : <LoadingSpinner width="2em" />}
+          <EditorContent className="rich-text" editor={editor} />
 
-        {call_to_action ? (
-          <div className="grid">
-            <p>
-              <a
-                className="button accent"
-                href={call_to_action.url}
-                target="_blank"
-                style={{ whiteSpace: "normal" }}
-                rel="noopener noreferrer"
-              >
-                {call_to_action.text}
-              </a>
-            </p>
-          </div>
-        ) : null}
-      </article>
-    </main>
+          {call_to_action ? (
+            <div className="grid">
+              <p>
+                <a
+                  className="button accent"
+                  href={call_to_action.url}
+                  target="_blank"
+                  style={{ whiteSpace: "normal" }}
+                  rel="noopener noreferrer"
+                >
+                  {call_to_action.text}
+                </a>
+              </p>
+            </div>
+          ) : null}
+        </div>
+        <BackToBlog />
+      </div>
+    </div>
   );
 };
 
