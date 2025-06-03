c8108d0584cf1f20a3fbd5a556f7365f29150e20
Ershad Kunnakkadan <ershad92@gmail.com>
2025-06-03 16:00:24 -0400

    refactor

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -188,10 +188,8 @@ class Admin::UsersController < Admin::BaseController
         amount_cents: credit_amount_cents,
         crediting_user: current_user
       )
-      if user_credit.save
-        user_credit.notify_user if credit_amount_cents > 0
-        return render json: { success: true, amount: params[:credit][:credit_amount] }
-      end
+      user_credit.notify_user if credit_amount_cents > 0
+      return render json: { success: true, amount: params[:credit][:credit_amount] }
     end
 
     render json: { success: false }
