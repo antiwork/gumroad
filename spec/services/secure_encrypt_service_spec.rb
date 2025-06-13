7337f3b394f6a4ec38ccd33a17bb672140e9c867
Ershad Kunnakkadan <ershad92@gmail.com>
2025-06-13 14:46:50 -0400

    refactor

--- spec/services/secure_encrypt_service_spec.rb
+++ spec/services/secure_encrypt_service_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 
 RSpec.describe SecureEncryptService do
   let(:key) { SecureRandom.random_bytes(32) }
-  let(:data) { 'this is a secret message' }
+  let(:text) { 'this is a secret message' }
 
   before do
     allow(GlobalConfig).to receive(:get).with('SECURE_ENCRYPT_KEY').and_return(key)
@@ -13,27 +13,27 @@ RSpec.describe SecureEncryptService do
   end
 
   describe '.encrypt' do
-    it 'encrypts data' do
-      encrypted_data = described_class.encrypt(data)
-      expect(encrypted_data).not_to be_blank
-      expect(encrypted_data).not_to eq(data)
+    it 'encrypts text' do
+      encrypted_text = described_class.encrypt(text)
+      expect(encrypted_text).not_to be_blank
+      expect(encrypted_text).not_to eq(text)
     end
   end
 
   describe '.decrypt' do
-    let(:encrypted_data) { described_class.encrypt(data) }
+    let(:encrypted_text) { described_class.encrypt(text) }
 
-    it 'decrypts data' do
-      expect(described_class.decrypt(encrypted_data)).to eq(data)
+    it 'decrypts text' do
+      expect(described_class.decrypt(encrypted_text)).to eq(text)
     end
 
-    it 'returns nil for tampered data' do
-      tampered_data = encrypted_data + 'tamper'
-      expect(described_class.decrypt(tampered_data)).to be_nil
+    it 'returns nil for tampered text' do
+      tampered_text = encrypted_text + 'tamper'
+      expect(described_class.decrypt(tampered_text)).to be_nil
     end
 
     it 'returns nil for a different key' do
-      encrypted_with_first_key = described_class.encrypt(data)
+      encrypted_with_first_key = described_class.encrypt(text)
 
       different_key = SecureRandom.random_bytes(32)
       allow(GlobalConfig).to receive(:get).with('SECURE_ENCRYPT_KEY').and_return(different_key)
@@ -44,23 +44,23 @@ RSpec.describe SecureEncryptService do
   end
 
   describe '.verify' do
-    let(:encrypted_data) { described_class.encrypt(data) }
+    let(:encrypted_text) { described_class.encrypt(text) }
 
-    it 'returns true for correct data' do
-      expect(described_class.verify(encrypted_data, data)).to be true
+    it 'returns true for correct text' do
+      expect(described_class.verify(encrypted_text, text)).to be true
     end
 
-    it 'returns false for incorrect data' do
-      expect(described_class.verify(encrypted_data, 'wrong message')).to be false
+    it 'returns false for incorrect text' do
+      expect(described_class.verify(encrypted_text, 'wrong message')).to be false
     end
 
-    it 'returns false for tampered encrypted data' do
-      tampered_data = encrypted_data + 'tamper'
-      expect(described_class.verify(tampered_data, data)).to be false
+    it 'returns false for tampered encrypted text' do
+      tampered_text = encrypted_text + 'tamper'
+      expect(described_class.verify(tampered_text, text)).to be false
     end
 
     it 'returns false for nil user input' do
-      expect(described_class.verify(encrypted_data, nil)).to be false
+      expect(described_class.verify(encrypted_text, nil)).to be false
     end
   end
 
@@ -71,12 +71,12 @@ RSpec.describe SecureEncryptService do
 
     it 'raises MissingKeyError if key is not set' do
       allow(GlobalConfig).to receive(:get).with('SECURE_ENCRYPT_KEY').and_return(nil)
-      expect { described_class.encrypt(data) }.to raise_error(SecureEncryptService::MissingKeyError, 'SECURE_ENCRYPT_KEY is not set.')
+      expect { described_class.encrypt(text) }.to raise_error(SecureEncryptService::MissingKeyError, 'SECURE_ENCRYPT_KEY is not set.')
     end
 
     it 'raises InvalidKeyError if key is not 32 bytes' do
       allow(GlobalConfig).to receive(:get).with('SECURE_ENCRYPT_KEY').and_return('short_key')
-      expect { described_class.encrypt(data) }.to raise_error(SecureEncryptService::InvalidKeyError, 'SECURE_ENCRYPT_KEY must be 32 bytes for aes-256-gcm.')
+      expect { described_class.encrypt(text) }.to raise_error(SecureEncryptService::InvalidKeyError, 'SECURE_ENCRYPT_KEY must be 32 bytes for aes-256-gcm.')
     end
   end
 end
