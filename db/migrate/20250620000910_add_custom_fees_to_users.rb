ec79e03ec05ca5921fd59a9ecc6b6aa3e97fce4e
Hanaffi <ahmedellord56@gmail.com>
2025-06-20 03:14:29 +0300

    Add custom fee percentage columns to users table

---
+++ db/migrate/20250620000910_add_custom_fees_to_users.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class AddCustomFeesToUsers < ActiveRecord::Migration[7.1]
+  def change
+    change_table :users, bulk: true do |t|
+      t.decimal :custom_direct_fee_percentage, precision: 5, scale: 2, default: nil
+      t.decimal :custom_discover_fee_percentage, precision: 5, scale: 2, default: nil
+    end
+  end
+end
