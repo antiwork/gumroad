14bb5551d0eff6667cf468e3f6674000ea1db022
Josef Strzibny <strzibny@strzibny.name>
2025-06-26 23:39:36 +0200

    Remove unnecessary conversion

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -325,12 +325,7 @@ class LinksController < ApplicationController
     authorize @product
     begin
       ActiveRecord::Base.transaction do
-        permitted_params = product_permitted_params.to_h
-        if permitted_params.key?(:hide_sold_out_variants)
-          val = permitted_params[:hide_sold_out_variants]
-          permitted_params[:hide_sold_out_variants] = val.to_s == 'true' || val.to_s == '1'
-        end
-        @product.assign_attributes(permitted_params.except(
+        @product.assign_attributes(product_permitted_params.except(
           :products,
           :description,
           :cancellation_discount,
