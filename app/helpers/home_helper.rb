3a86a1be1f2ff0d2a7954dec30f23c6fd2822e06
Piotr Papiernik <piotrpapiernik132@gmail.com>
2025-06-30 09:29:40 +0200

    fix(about page): adjust searchmarketplace button; add links to creators

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -38,25 +38,29 @@ module HomeHelper
         quote: "I launched MaxPacks as an experimental side gig; but within 2 years those Procreate brushes were earning more than my 6-figure salary in CG. Leaving in favor of Gumroad enabled me to explore other aspects of my art, develop new hobbies, and finally prioritize my personal life.",
         avatar_path: "creators/maxulichney-round.svg",
         name: "Max Ulichney",
-        description: "Sells Procreate brush packs"
+        description: "Sells Procreate brush packs",
+        url: "https://maxulichney.gumroad.com/?recommended_by=search"
       },
       {
         quote: "For years, I had a goal to develop 'passive' income streams, but struggled to make that a reality. Last year, I started selling informational products on Gumroad and since then have made $10k+ per month building products that I love.",
         avatar_path: "creators/stephsmith-round.svg",
         name: "Steph Smith",
-        description: "Sells content tutorials"
+        description: "Sells content tutorials",
+        url: "https://stephsmithio.gumroad.com/?recommended_by=search"
       },
       {
         quote: "Originally, I took pre-orders for my Trend Reports on Gumroad. But I received... exactly $0. So I changed tactics: I made half of my report free, and the other half paid. Today, 99% of Trends.VC revenue is recurring in the form of annual and quarterly subscriptions.",
         avatar_path: "creators/trendsvc-round.svg",
         name: "trendsvc",
-        description: "Sells business insights and expertise"
+        description: "Sells business insights and expertise",
+        url: "https://trendsvc.gumroad.com/?recommended_by=search"
       },
       {
         quote: "I love Gumroad because it can't be any simpler. I upload a file, set a price, and I can start selling on the internet. The money I make from my sales lands directly in my bank account every Friday.",
         avatar_path: "creators/danielvassalo-round.svg",
         name: "Daniel Vassallo",
-        description: "Sells business insights and expertise"
+        description: "Sells business insights and expertise",
+        url: "https://danielvassalo.gumroad.com/?recommended_by=search"
       }
     ]
   end
