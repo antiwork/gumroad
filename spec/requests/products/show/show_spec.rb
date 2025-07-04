7f14e7b5ae2a0b3c05f612940cc41f332946123c
Kyle <github@kylezh.com>
2025-07-04 11:24:34 -0400

    Update spec setup & use accessible helpers

--- spec/requests/products/show/show_spec.rb
+++ spec/requests/products/show/show_spec.rb
@@ -614,30 +614,24 @@ describe("ProductShowScenario", type: :feature, js: true) do
     let!(:creator) { create(:named_user) }
     let!(:product) { create(:product, user: creator, name: "Test Product with Variants", hide_sold_out_variants: false) }
     let!(:variant_category) { create(:variant_category, link: product) }
-    let!(:sold_out_variant) { create(:variant, name: "Starter", variant_category: variant_category) }
     let!(:available_variant) { create(:variant, name: "Standard", variant_category: variant_category) }
-
-    before do
-      sold_out_variant.update!(max_purchase_count: 1)
-      price = create(:variant_price, variant: sold_out_variant)
-      purchase = create(:purchase, link: product, price_id: price.id)
-      purchase.variant_attributes << sold_out_variant
-      login_as(creator)
-    end
+    let!(:sold_out_variant) { create(:variant, name: "Starter", variant_category: variant_category, max_purchase_count: 1) }
+    let!(:sold_out_variant_purchase) { create(:purchase, link: product, variant_attributes: [sold_out_variant]) }
 
     it "disables or hides the variant based on the hide_sold_out_variants setting" do
       # The sold out variant should be visible, but in a disabled state.
-      visit "/l/#{product.unique_permalink}"
-      expect(page).to have_css("button[aria-label='#{sold_out_variant.name}'][disabled]")
-      expect(page).to have_css("button[aria-label='#{available_variant.name}']:not([disabled])")
+      product.update!(hide_sold_out_variants: false)
+
+      visit product.long_url
+      expect(page).to have_radio_button(sold_out_variant.name, disabled: true)
+      expect(page).to have_radio_button(available_variant.name, disabled: false)
 
       # The sold out variant should not be visible on the page.
-      product.update_attribute(:hide_sold_out_variants, true)
+      product.update!(hide_sold_out_variants: true)
 
-      visit "/l/#{product.unique_permalink}"
-      expect(page).not_to have_css("button[aria-label='#{sold_out_variant.name}']")
-      expect(page).not_to have_css("button[aria-label='#{sold_out_variant.name}'][disabled]")
-      expect(page).to have_css("button[aria-label='#{available_variant.name}']:not([disabled])")
+      visit product.long_url
+      expect(page).not_to have_radio_button(sold_out_variant.name)
+      expect(page).to have_radio_button(available_variant.name, disabled: false)
     end
   end
 end
