47357c62742a680527c31b158339ad3c90910bd7
Hanaffi <ahmedellord56@gmail.com>
2025-07-10 21:39:13 +0300

    Refactor Product Card components to use Intersection Observer for lazy loading images and enhance Thumbnail component with loading and fetch priority attributes

--- app/javascript/components/Product/Card.tsx
+++ app/javascript/components/Product/Card.tsx
@@ -1,6 +1,7 @@
 import cx from "classnames";
 import * as React from "react";
 
+import useIntersectionObserver from "$app/hooks/useIntersectionObserver";
 import { CardProduct, Ratings } from "$app/parsers/product";
 import { formatOrderOfMagnitude } from "$app/utils/formatOrderOfMagnitude";
 
@@ -17,55 +18,25 @@ export const Card = ({
   product: CardProduct;
   badge?: React.ReactNode;
   footerAction?: React.ReactNode;
-}) => (
-  <article className="product-card">
-    <figure>
-      <Thumbnail url={product.thumbnail_url} nativeType={product.native_type} />
-    </figure>
-    {product.quantity_remaining != null ? <div className="ribbon">{`${product.quantity_remaining} left`}</div> : null}
-    <header>
-      <a href={product.url} className="stretched-link">
-        <h4 itemProp="name">{product.name}</h4>
-      </a>
-      {product.seller ? (
-        <AuthorByline
-          name={product.seller.name}
-          profileUrl={product.seller.profile_url}
-          avatarUrl={product.seller.avatar_url ?? undefined}
-        />
-      ) : null}
-      {product.ratings?.count ? <Rating ratings={product.ratings} /> : null}
-    </header>
-    <footer>
-      <PriceTag
-        url={product.url}
-        currencyCode={product.currency_code}
-        price={product.price_cents}
-        isPayWhatYouWant={product.is_pay_what_you_want}
-        isSalesLimited={product.is_sales_limited}
-        recurrence={
-          product.recurrence ? { id: product.recurrence, duration_in_months: product.duration_in_months } : undefined
-        }
-        creatorName={product.seller?.name}
-      />
-      {footerAction}
-    </footer>
-    {badge}
-  </article>
-);
+}) => {
+  const cardRef = React.useRef<HTMLImageElement | null>(null);
+  const visible = useIntersectionObserver(cardRef, { freezeOnceVisible: true });
 
-export const HorizontalCard = ({ product, big }: { product: CardProduct; big?: boolean }) => (
-  <article className={cx("product-card horizontal", { big })} style={{ position: "relative" }}>
-    <figure>
-      <Thumbnail url={product.thumbnail_url} nativeType={product.native_type} />
-    </figure>
-    {product.quantity_remaining != null ? <div className="ribbon">{product.quantity_remaining} left</div> : null}
-    <section>
+  return (
+    <article ref={cardRef} className="product-card">
+      <figure>
+        <Thumbnail
+          url={product.thumbnail_url}
+          nativeType={product.native_type}
+          loading={visible ? "eager" : "lazy"}
+          fetchPriority={visible ? "high" : "auto"}
+        />
+      </figure>
+      {product.quantity_remaining != null ? <div className="ribbon">{`${product.quantity_remaining} left`}</div> : null}
       <header>
-        <a href={product.url} className="stretched-link" draggable="false">
-          {big ? <h2 itemProp="name">{product.name}</h2> : <h3 itemProp="name">{product.name}</h3>}
+        <a href={product.url} className="stretched-link">
+          <h4 itemProp="name">{product.name}</h4>
         </a>
-        <small>{product.description}</small>
         {product.seller ? (
           <AuthorByline
             name={product.seller.name}
@@ -73,6 +44,7 @@ export const HorizontalCard = ({ product, big }: { product: CardProduct; big?: b
             avatarUrl={product.seller.avatar_url ?? undefined}
           />
         ) : null}
+        {product.ratings?.count ? <Rating ratings={product.ratings} /> : null}
       </header>
       <footer>
         <PriceTag
@@ -86,11 +58,62 @@ export const HorizontalCard = ({ product, big }: { product: CardProduct; big?: b
           }
           creatorName={product.seller?.name}
         />
-        {product.ratings?.count ? <Rating ratings={product.ratings} /> : null}
+        {footerAction}
       </footer>
-    </section>
-  </article>
-);
+      {badge}
+    </article>
+  );
+};
+
+export const HorizontalCard = ({ product, big }: { product: CardProduct; big?: boolean }) => {
+  const cardRef = React.useRef<HTMLImageElement | null>(null);
+  const visible = useIntersectionObserver(cardRef, { freezeOnceVisible: true });
+
+  return (
+    <article ref={cardRef} className={cx("product-card horizontal", { big })} style={{ position: "relative" }}>
+      <figure>
+        <Thumbnail
+          url={product.thumbnail_url}
+          nativeType={product.native_type}
+          loading={visible ? "eager" : "lazy"}
+          fetchPriority={visible ? "high" : "auto"}
+        />
+      </figure>
+      {product.quantity_remaining != null ? <div className="ribbon">{product.quantity_remaining} left</div> : null}
+      <section>
+        <header>
+          <a href={product.url} className="stretched-link" draggable="false">
+            {big ? <h2 itemProp="name">{product.name}</h2> : <h3 itemProp="name">{product.name}</h3>}
+          </a>
+          <small>{product.description}</small>
+          {product.seller ? (
+            <AuthorByline
+              name={product.seller.name}
+              profileUrl={product.seller.profile_url}
+              avatarUrl={product.seller.avatar_url ?? undefined}
+            />
+          ) : null}
+        </header>
+        <footer>
+          <PriceTag
+            url={product.url}
+            currencyCode={product.currency_code}
+            price={product.price_cents}
+            isPayWhatYouWant={product.is_pay_what_you_want}
+            isSalesLimited={product.is_sales_limited}
+            recurrence={
+              product.recurrence
+                ? { id: product.recurrence, duration_in_months: product.duration_in_months }
+                : undefined
+            }
+            creatorName={product.seller?.name}
+          />
+          {product.ratings?.count ? <Rating ratings={product.ratings} /> : null}
+        </footer>
+      </section>
+    </article>
+  );
+};
 
 const Rating = ({ ratings, style }: { ratings: Ratings; style?: React.CSSProperties }) => (
   <div className="rating" aria-label="Rating" style={style}>
--- app/javascript/components/Product/Thumbnail.tsx
+++ app/javascript/components/Product/Thumbnail.tsx
@@ -5,5 +5,19 @@ import { ProductNativeType } from "$app/parsers/product";
 
 const nativeTypeThumbnails = require.context("$assets/images/native_types/thumbnails/");
 
-export const Thumbnail = ({ url, nativeType }: { url: string | null; nativeType: ProductNativeType }) =>
-  url ? <img src={url} /> : <img src={cast(nativeTypeThumbnails(`./${nativeType}.svg`))} />;
+export const Thumbnail = ({
+  url,
+  nativeType,
+  loading,
+  fetchPriority = "auto",
+}: {
+  url: string | null;
+  nativeType: ProductNativeType;
+  loading?: "eager" | "lazy";
+  fetchPriority?: "high" | "low" | "auto";
+}) =>
+  url ? (
+    <img src={url} loading={loading} fetchPriority={fetchPriority} />
+  ) : (
+    <img src={cast(nativeTypeThumbnails(`./${nativeType}.svg`))} loading={loading} fetchPriority={fetchPriority} />
+  );
