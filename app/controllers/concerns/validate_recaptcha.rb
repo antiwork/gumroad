6c4042e8ad9f44229a6a8df45c529032f4059688
Udit Takkar <udit222001@gmail.com>
2025-07-14 04:29:44 +0530

    chore: remove

--- app/controllers/concerns/validate_recaptcha.rb
+++ app/controllers/concerns/validate_recaptcha.rb
@@ -28,7 +28,6 @@ module ValidateRecaptcha
 
     def valid_recaptcha_response?(site_key:)
       return true if Rails.env.test?
-      return true
 
       verification_response = recaptcha_verification_response(site_key:)
       verification_response.dig("tokenProperties", "valid")
