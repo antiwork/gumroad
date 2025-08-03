bde24782ef70db099fb67da9e32c8fdacb7d4c11
Jash Ambaliya <87864439+AJ0070@users.noreply.github.com>
2025-08-03 09:40:33 +0530

    minor change

--- app/javascript/components/server-components/Audience/CustomersPage.tsx
+++ app/javascript/components/server-components/Audience/CustomersPage.tsx
@@ -420,7 +420,7 @@ const CustomersPage = ({
                   e.currentTarget.submit();
                 }}
               >
-                <button type="submit" className="button primary">
+                <button type="submit" color="primary">
                   Download
                 </button>
               </form>
