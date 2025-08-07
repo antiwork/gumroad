debd4cccbc3cd94a4fd9711583e6c2e98800c9dc
Kyle <github@kylezh.com>
2025-08-07 12:48:28 -0400

    Fix price retrieval to filter by current currency
    When a product's currency is changed multiple times (e.g., USD → EUR →
    USD), we correctly creates separate Price records for each currency.
    
    But retrieval methods (e.g. Link#default_price) return incorrect values
    because it does not consider the current currency, potentially returning
    the currency from one record, and dollar value from another.

--- app/modules/product/prices.rb
+++ app/modules/product/prices.rb
@@ -26,13 +26,13 @@ module Product::Prices
   def rental_price_cents
     return read_attribute(:rental_price_cents) unless persisted?
 
-    rentable? ? alive_prices.select(&:is_rental?).last.price_cents : nil
+    rentable? ? alive_prices.where(currency: price_currency_type).select(&:is_rental?).last&.price_cents : nil
   end
 
   def default_price
-    return alive_prices.select(&:is_rental?).last if rent_only?
+    return alive_prices.where(currency: price_currency_type).select(&:is_rental?).last if rent_only?
 
-    relevant_prices = alive_prices.select(&:is_buy?)
+    relevant_prices = alive_prices.where(currency: price_currency_type).select(&:is_buy?)
     relevant_prices = relevant_prices.select(&:is_default_recurrence?) if is_recurring_billing && subscription_duration.present?
     relevant_prices.last
   end
