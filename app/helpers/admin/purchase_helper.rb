fd59a3602942523861baa0c88c6417c01ef25d58
Razvan Marescu <razvan@marescu.net>
2025-08-28 23:48:09 -0400

    Use helper method

---
+++ app/helpers/admin/purchase_helper.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+module Admin::PurchaseHelper
+  def purchase_states(purchase)
+    [
+      purchase.purchase_state.capitalize,
+      ("(refunded)" if purchase.stripe_refunded?),
+      ("(partially refunded)" if purchase.stripe_partially_refunded?),
+      ("(chargeback)" if purchase.chargedback_not_reversed?),
+      ("(chargeback reversed)" if purchase.chargeback_reversed?),
+      purchase_error_code(purchase),
+    ].compact
+  end
+
+  def purchase_error_code(purchase)
+    return unless purchase.failed?
+
+    formatted_error_code = purchase.formatted_error_code
+
+    last_chargebacked_purchase = find_last_chargebacked_purchase_for(purchase) if purchase.error_code == PurchaseErrorCode::BUYER_CHARGED_BACK
+
+    if last_chargebacked_purchase.present?
+      "(#{link_to(formatted_error_code, admin_purchase_path(last_chargebacked_purchase))})".html_safe
+    else
+      "(#{formatted_error_code})"
+    end
+  end
+
+  def find_last_chargebacked_purchase_for(purchase)
+    return nil unless purchase.email.present? || purchase.browser_guid.present?
+
+    Purchase.chargedback
+            .not_chargeback_reversed
+            .where("email = ? OR browser_guid = ?", purchase.email, purchase.browser_guid)
+            .order(chargeback_date: :desc)
+            .first
+  end
+end
