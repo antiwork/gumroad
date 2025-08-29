bb0ba51b368b36ca7d5f7cedef19297543c91fd6
Smit <smitp.contact@gmail.com>
2025-08-30 04:05:32 +0530

    refactor(PaymentForm): fix form field position to avoid layout shift

--- app/javascript/components/Checkout/PaymentForm.tsx
+++ app/javascript/components/Checkout/PaymentForm.tsx
@@ -596,7 +596,6 @@ const CustomerDetails = () => {
           </div>
         </div>
       ) : null}
-      {isTippingEnabled(state) ? <TipSelector /> : null}
       {state.products.length === 1 && state.products[0]?.canGift && !state.products[0]?.payInInstallments ? (
         <GiftForm isMembership={state.products[0]?.nativeType === "membership"} />
       ) : null}
@@ -1016,7 +1015,6 @@ const PayPal = () => {
   return (
     <>
       <SharedInputs />
-      {isTippingEnabled(state) ? <TipSelector /> : null}
       <div>
         {nativePaypal && implementation === "native" ? (
           <NativePayPal implementation={nativePaypal} />
@@ -1161,7 +1159,6 @@ const StripePaymentRequest = () => {
   return (
     <>
       <SharedInputs />
-      {isTippingEnabled(state) ? <TipSelector /> : null}
       <div>
         <Button color="primary" onClick={() => dispatch({ type: "offer" })} disabled={isSubmitDisabled(state)}>
           {payLabel}
@@ -1224,14 +1221,12 @@ export const PaymentForm = ({
           </div>
         </div>
       ) : null}
+      <EmailAddress />
+      {isTippingEnabled(state) ? <TipSelector /> : null}
       {isTestPurchase || !requiresPayment(state) ? (
-        <>
-          <EmailAddress />
-          <FreePurchase />
-        </>
+        <FreePurchase />
       ) : (
         <>
-          <EmailAddress />
           <div>
             <div className="paragraphs">
               <h4>Pay with</h4>
