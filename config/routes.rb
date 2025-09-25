dfca01f020b7ea1cb7182dae40cf5d1e380bc309
Kyle <github@kylezh.com>
2025-09-25 14:15:30 -0400

    squash with controller

--- config/routes.rb
+++ config/routes.rb
@@ -481,10 +481,13 @@ Rails.application.routes.draw do
         get :paged, on: :collection
         get :cart_item, on: :collection
         get :statistics, on: :member
+
+        scope module: :upsells do
+          resource :pause, only: [:create, :destroy]
+        end
       end
       namespace :upsells do
         resources :products, only: [:index, :show]
-        resource :pause, only: [:create, :destroy]
       end
       resource :form, only: %i[show update], controller: :form
     end
