52c21a19589209e208a2361cb5aa284721ba8445
saurabh <saurabhrai1717@gmail.com>
2025-09-30 19:17:57 +0530

    migrate subscribe-preview

--- app/javascript/components/server-components/SubscribePreview.tsx
+++ app/javascript/components/server-components/SubscribePreview.tsx
@@ -11,11 +11,11 @@ type Props = {
 };
 
 export const SubscribePreview = ({ avatar_url, title }: Props) => (
-  <div className="w-full h-full grid items-center p-8 gap-8" style={{ gridTemplateColumns: '27.5% 1fr' }}>
+  <div className="w-full h-full grid items-center p-7 gap-8" style={{ gridTemplateColumns: '27.5% 1fr' }}>
     <img className="w-full aspect-square border border-gray-300 flex-shrink-0" style={{ borderRadius: '10rem' }} src={avatar_url} />
-    <div className="grid gap-3">
+    <div className="ml-1.5">
       <span className="logo-full text-sm opacity-20" />
-      <h1 className="text-3xl leading-tight overflow-hidden" style={{ display: '-webkit-box', WebkitLineClamp: 2, WebkitBoxOrient: 'vertical', fontSize: '2rem' }}>{title}</h1>
+      <h1 className="text-3xl leading-tight overflow-hidden mb-3 mt-3" style={{ display: '-webkit-box', WebkitLineClamp: 2, WebkitBoxOrient: 'vertical', fontSize: '2rem' }}>{title}</h1>
       <Button color="accent" className="w-fit">Subscribe</Button>
     </div>
   </div>
