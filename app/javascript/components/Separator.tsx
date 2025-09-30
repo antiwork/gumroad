3e04b1cdea64f6bc14aa6a29d852e7495f186ba3
Hanaffi <ahmedellord56@gmail.com>
2025-10-01 01:23:26 +0300

    adjust for more flexibility

--- app/javascript/components/Separator.tsx
+++ app/javascript/components/Separator.tsx
@@ -1,10 +1,19 @@
 import * as React from "react";
 
-export const Separator = ({ children, ...rest }: React.PropsWithChildren<React.HTMLAttributes<HTMLDivElement>>) => (
+import { classNames } from "$app/utils/classNames";
+
+export const Separator = ({
+  children,
+  className,
+  ...rest
+}: React.PropsWithChildren<React.HTMLAttributes<HTMLDivElement>>) => (
   <div
     {...rest}
     role="separator"
-    className="override grid grid-cols-[1fr_auto_1fr] items-center gap-3 before:content-[''] before:[border-bottom:solid_.0625rem_rgb(var(--parent-color)/var(--border-alpha))] after:content-[''] after:[border-bottom:solid_.0625rem_rgb(var(--parent-color)/var(--border-alpha))]"
+    className={classNames(
+      "override grid grid-cols-[1fr_auto_1fr] items-center gap-3 before:content-[''] before:[border-bottom:solid_.0625rem_rgb(var(--parent-color)/var(--border-alpha))] after:content-[''] after:[border-bottom:solid_.0625rem_rgb(var(--parent-color)/var(--border-alpha))]",
+      className,
+    )}
   >
     {children}
   </div>
