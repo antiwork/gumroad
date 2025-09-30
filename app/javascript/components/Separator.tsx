06ac7708daa8186011c9d66e11ba84376a6483f0
Hanaffi <ahmedellord56@gmail.com>
2025-09-30 23:39:35 +0300

    Refactor: Update Separator component styles and remove unused SCSS files

--- app/javascript/components/Separator.tsx
+++ app/javascript/components/Separator.tsx
@@ -4,7 +4,7 @@ export const Separator = ({ children, ...rest }: React.PropsWithChildren<React.H
   <div
     {...rest}
     role="separator"
-    className="before:border-gray-300 after:border-gray-300 grid grid-cols-[1fr_auto_1fr] items-center gap-3 before:border-b before:content-[''] after:border-b after:content-['']"
+    className="override grid grid-cols-[1fr_auto_1fr] items-center gap-3 before:border-b before:border-solid before:border-[rgb(var(--parent-color)/var(--border-alpha))] before:content-[''] after:border-b after:border-solid after:border-[rgb(var(--parent-color)/var(--border-alpha))] after:content-['']"
   >
     {children}
   </div>
