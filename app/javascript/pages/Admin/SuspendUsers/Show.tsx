9d006c88093c35a44ca79f9d5fc4b5242eac762e
Emmanuel Cousin <emmanuel@hey.com>
2025-10-10 16:19:01 -0300

    Add Admin SuspendUsers feature
    - Convert SuspendUsers controller to use Inertia.js
    - Add React components for SuspendUsers page (Show and Form)
    - Update controller spec for Inertia rendering
    - Add strong parameters for suspend_users params
    - Use admin layout for SuspendUsers show action
    - Simplifies the fallback mechanism to leave ERB templates as-is, and not to suffix them with _old
    - Use Inertia helpers in Rspec

---
+++ app/javascript/pages/Admin/SuspendUsers/Show.tsx
@@ -0,0 +1,23 @@
+import { usePage, Head } from "@inertiajs/react";
+import React from "react";
+
+import Form from "./Form";
+
+type Props = {
+  suspend_reasons: string[];
+  authenticity_token: string;
+  title: string;
+};
+
+const AdminSuspendUsers = () => {
+  const { suspend_reasons, authenticity_token, title } = usePage<Props>().props;
+
+  return (
+    <>
+      <Head title={title} />
+      <Form authenticity_token={authenticity_token} suspend_reasons={suspend_reasons} />
+    </>
+  );
+};
+
+export default AdminSuspendUsers;
