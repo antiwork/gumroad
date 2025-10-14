b4e80e5059ef3c209b708d097fe661b9fbfaa759
Jono Mingard <reason.koan@gmail.com>
2025-10-14 03:08:37 +0100

    Fix

--- app/javascript/components/ui/ProductCardGrid.tsx
+++ app/javascript/components/ui/ProductCardGrid.tsx
@@ -9,6 +9,7 @@ export const ProductCardGrid = React.forwardRef<HTMLDivElement, React.HTMLProps<
         ref={ref}
         className={classNames(
           "grid grid-cols-2 gap-4 @xl:grid-cols-3 @3xl:grid-cols-4 @4xl:grid-cols-5",
+          // Adding !important as the order of style rules Tailwind creates means these won't necessarily be higher precedence
           !narrow && "lg:grid-cols-2! lg:@3xl:grid-cols-3! lg:@5xl:grid-cols-4! lg:@7xl:grid-cols-5!",
           className,
         )}
