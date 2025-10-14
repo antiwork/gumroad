1d228acabc5622d5c94f87fcdb4001bbf0aaf545
Rachel-splatt <rachelplatt625@gmail.com>
2025-10-14 13:00:33 -0400

    add grid-cols-sidebar

--- app/javascript/components/server-components/LibraryPage.tsx
+++ app/javascript/components/server-components/LibraryPage.tsx
@@ -382,7 +382,7 @@ const LibraryPage = ({ results, creators, bundles, reviews_page_enabled, followi
             </span>
           </div>
         ) : null}
-        <div className="grid auto-cols-[3fr] grid-cols-1 items-start gap-x-16 gap-y-8 lg:grid-flow-col">
+        <div className="grid grid-cols-1 items-start gap-x-16 gap-y-8 lg:grid-cols-[var(--grid-cols-sidebar)]">
           {!showArchivedNotice && (hasParams || archivedCount > 0 || state.results.length > 9) ? (
             <div className="stack">
               <header>
