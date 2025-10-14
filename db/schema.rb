9b86541a8077f393e5627230ec161e490a84a4e1
Pradumn <pradumn@tealfeed.com>
2025-10-14 12:57:15 +0530

    chore

--- db/schema.rb
+++ db/schema.rb
@@ -2718,5 +2718,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_10_03_165816) do
     t.index ["zip_code"], name: "index_zip_tax_rates_on_zip_code"
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
