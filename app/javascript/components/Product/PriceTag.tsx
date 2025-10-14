e564ebebddd22bc3d85833fd07f0eea9ec6de606
saurabh <saurabhrai1717@gmail.com>
2025-10-15 00:15:51 +0530

    migrate _price.scss to tailwind

--- app/javascript/components/Product/PriceTag.tsx
+++ app/javascript/components/Product/PriceTag.tsx
@@ -60,7 +60,7 @@ export const PriceTag = ({
     <div itemScope itemProp="offers" itemType="https://schema.org/Offer" className="flex items-center">
       <div className={`has-tooltip ${tooltipPosition}`} aria-describedby={tooltipUid}>
         <div
-          className="price"
+          className="block py-1 px-2 pr-[calc(0.5rem+1em)] relative border border-r-0 bg-gradient-to-l from-transparent from-[1em] to-pink to-[1em] bg-no-repeat overflow-hidden whitespace-nowrap text-ellipsis text-black before:content-[''] before:absolute before:top-0 before:bottom-0 before:right-0 before:border-solid before:border-[calc(0.25rem+0.5lh)] before:border-l-[0.0625rem] before:border-r-[1em] before:border-r-transparent before:border-l-border before:border-t-border before:border-b-border after:content-[''] after:absolute after:top-0 after:bottom-0 after:right-[0.0625rem] after:border-solid after:border-[calc(0.25rem+0.5lh)] after:border-l-0 after:border-r-[1em] after:border-r-transparent after:border-pink"
           itemProp="price"
           content={formatPriceCentsWithoutCurrencySymbolAndComma(currencyCode, price)}
         >
