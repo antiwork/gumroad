2e6768116f18c16cef1409b1ce5ceda08cdb3857
Aavishkar Kolte <aavishkarkolte@gmail.com>
2025-10-15 17:33:23 +0530

    add tailwind classes to Modal.tsx

--- app/javascript/components/Modal.tsx
+++ app/javascript/components/Modal.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { classNames } from "$app/utils/classNames";
 
 export const Modal = ({
   open,
@@ -38,6 +39,14 @@ export const Modal = ({
 
   return (
     <dialog
+      className={classNames("bg-background text-foreground",
+        "border-solid border border-border rounded-sm",
+        "shadow-lg shadow-foreground",
+        "p-8 flex flex-col fixed top-1/2 left-1/2 -translate-1/2 -translate-y-1/2",
+        "z-20 w-fit min-w-xs max-w-[43.75rem] gap-4",
+        "dark:shadow-none",
+        "backdrop:bg-black/80 not-open:hidden"
+      )}
       open={supportsNative ? undefined : open}
       ref={ref}
       onClick={(e) => {
@@ -56,13 +65,13 @@ export const Modal = ({
       aria-labelledby={id}
     >
       {title ? (
-        <h2 id={id}>
+        <h2 id={id} className={classNames("flex justify-between gap-4 items-start")}>
           {title}
-          {allowClose ? <button type="button" className="close" aria-label="Close" onClick={dispatchClose} /> : null}
+          {allowClose ? <button type="button" className={classNames("text-base/[1.4rem] after:text-base after:content-['\00a0'] after:inline after:bg-current after:min-h-[max(1lh,1em)] after:w-[1em] after:mask-center after:mask-size-[120%] after:mask-no-repeat after:shrink-0")} aria-label="Close" onClick={dispatchClose} /> : null}
         </h2>
       ) : null}
       {children}
-      {footer ? <footer>{footer}</footer> : null}
+      {footer ? <footer className={classNames("grid gap-4 sm:flex sm:justify-end")}>{footer}</footer> : null}
     </dialog>
   );
 };
