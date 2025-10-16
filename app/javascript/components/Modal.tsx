75c4777261eb181eeced51445d35cdda8d1b58b0
Maya <19721695+code-elf@users.noreply.github.com>
2025-10-16 22:24:11 +0200

    fix

--- app/javascript/components/Modal.tsx
+++ app/javascript/components/Modal.tsx
@@ -22,6 +22,7 @@ export const Modal = ({
     <>
       <Dialog.Overlay className="fixed inset-0 z-10 bg-black/80" />
       <Dialog.Content
+        aria-modal={modal}
         className="bg-filled fixed top-[50%] left-[50%] z-20 flex max-w-175 min-w-80 translate-[-50%] flex-col gap-4 rounded border border-border p-8 shadow-lg dark:shadow-none"
         onOpenAutoFocus={(e) => {
           if (!modal) e.preventDefault();
