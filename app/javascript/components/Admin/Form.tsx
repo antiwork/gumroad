4916f001e8af1cdda35b2c4996559e0874e77879
Emmanuel Cousin <emmanuel@hey.com>
2025-10-21 09:47:27 -0300

    fix flash messages

--- app/javascript/components/Admin/Form.tsx
+++ app/javascript/components/Admin/Form.tsx
@@ -3,7 +3,7 @@ import { cast } from "ts-safe-cast";
 
 import { ResponseError, assertResponseError, request } from "$app/utils/request";
 
-import { showAlert } from "$app/components/server-components/Alert";
+import { useClientAlert } from "$app/components/ClientAlertProvider";
 
 export const Form = ({
   url,
@@ -21,6 +21,7 @@ export const Form = ({
   className?: string;
 }) => {
   const [isLoading, setIsLoading] = React.useState(false);
+  const { showAlert } = useClientAlert();
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
