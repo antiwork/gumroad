6d20a2ab300f00ba6dd86167490c5579ee1dc704
Harbaksh Chhabra <chhabra.harbaksh@gmail.com>
2025-10-23 14:14:20 +0530

    Exclude loan paydown from Credits head

--- spec/modules/user/stats_spec.rb
+++ spec/modules/user/stats_spec.rb
@@ -368,6 +368,8 @@ describe User::Stats, :vcr do
         5.times do
           Credit.create_for_financing_paydown!(purchase: create(:purchase, link: create(:product, user: @user)), amount_cents: -250, merchant_account: @user.stripe_account, stripe_loan_paydown_id: "cptxn_12345")
           Credit.create_for_credit!(user: @user, amount_cents: 1000, crediting_user: create(:user))
+          stub_const("GUMROAD_ADMIN_ID", create(:admin_user).id)
+          Credit.create_for_manual_paydown_on_stripe_loan!(amount_cents: -2000, merchant_account: @user.stripe_account, stripe_loan_paydown_id: "cptxn_#{SecureRandom.uuid}")
         end
       end
 
