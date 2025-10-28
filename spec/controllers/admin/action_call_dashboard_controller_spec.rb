decb23329e52f10ab5ccf58cac7f4b73bd6d312a
Emmanuel Cousin <emmanuel@hey.com>
2025-10-28 14:54:18 +0100

    rspec

--- spec/controllers/admin/action_call_dashboard_controller_spec.rb
+++ spec/controllers/admin/action_call_dashboard_controller_spec.rb
@@ -24,8 +24,7 @@ describe Admin::ActionCallDashboardController, type: :controller, inertia: true 
 
       expect(response).to have_http_status(:ok)
       expect_inertia.to render_component "Admin/ActionCallDashboard/Index"
-      expect_inertia.to have_exact_props(
-        admin_action_call_infos: [admin_action_call_info2, admin_action_call_info1].map do |info|
+      expect(inertia.props[:admin_action_call_infos]).to eq([admin_action_call_info2, admin_action_call_info1].map do |info|
           {
             id: info.id,
             controller_name: info.controller_name,
