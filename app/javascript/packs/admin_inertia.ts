82b7fdaa21bd8508fa4dcb2cde1195d4eb764d1e
Emmanuel Cousin <emmanuel@hey.com>
2025-10-31 16:15:15 +0100

    Update admin_inertia.ts

--- app/javascript/packs/admin_inertia.ts
+++ app/javascript/packs/admin_inertia.ts
@@ -64,6 +64,7 @@ const urlsMigratedtoInertia = [
   // Routes.admin_compliance_cards_url(),
   // Routes.admin_user_url(),
   new RegExp(Routes.admin_product_url("\\w+"), "u"),
+  /\/admin\/users\/\w+\/products/, // Routes.admin_user_products_url
   // Add other urls here when they are migrated to inertia
 ];
 
