41f5d0f47477d7cd721ac39c9dfc1ce2f9ebef8e
Emmanuel Cousin <emmanuel@hey.com>
2025-10-31 16:49:48 +0100

    Update products_controller.rb

--- app/controllers/admin/users/products_controller.rb
+++ app/controllers/admin/users/products_controller.rb
@@ -9,7 +9,7 @@ class Admin::Users::ProductsController < Admin::Users::BaseController
     @title = "#{@user.display_name} products on Gumroad"
 
     list_paginated_products user: @user,
-                            products: @user.products.order(Arel.sql(PRODUCTS_ORDER)),
+                            products: @user.products,
                             inertia_template: "Admin/Users/Products/Index"
   end
 end
