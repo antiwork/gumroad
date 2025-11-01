3ab0c388364798ac9f99f28a89ee8d0b47667975
Jono Mingard <reason.koan@gmail.com>
2025-11-01 15:01:41 +0000

    Fix

---
+++ app/javascript/pages/Admin/MerchantAccounts/Show.tsx
@@ -0,0 +1,14 @@
+import { usePage } from "@inertiajs/react";
+import React from "react";
+
+import AdminMerchantAccount, {
+  type AdminMerchantAccountProps,
+} from "$app/components/Admin/MerchantAccounts/MerchantAccount";
+
+const AdminMerchantAccountsShow = () => {
+  const { merchant_account } = usePage<{ merchant_account: AdminMerchantAccountProps }>().props;
+
+  return <AdminMerchantAccount merchant_account={merchant_account} />;
+};
+
+export default AdminMerchantAccountsShow;
