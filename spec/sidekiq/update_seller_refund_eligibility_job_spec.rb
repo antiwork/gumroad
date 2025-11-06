40af58f212a08d2e8abb582c258906e06110a89c
shanjairaj7 <shanjairaj7@users.noreply.github.com>
2025-11-06 10:59:44 +0530

    Auto-remove LowBalanceFraudCheck probation when balance exceeds 00

--- spec/sidekiq/update_seller_refund_eligibility_job_spec.rb
+++ spec/sidekiq/update_seller_refund_eligibility_job_spec.rb
@@ -52,4 +52,42 @@ describe UpdateSellerRefundEligibilityJob do
       expect { perform }.to change { user.reload.refunds_disabled? }.from(false).to(true)
     end
   end
+
+  context "when user is on probation due to LowBalanceFraudCheck" do
+    before do
+      user.put_on_probation(author_name: User::LowBalanceFraudCheck::LOW_BALANCE_FRAUD_CHECK_AUTHOR_NAME, content: "Test probation")
+      user.disable_refunds!
+    end
+
+    it "removes probation and marks compliant when balance exceeds $100" do
+      # Current balance is $99.99
+      create(:balance, user: user, amount_cents: 99_99)
+      expect { perform }.not_to change { user.reload.user_risk_state }
+
+      # Current balance is $100
+      create(:balance, user: user, amount_cents: 1)
+      expect { perform }.not_to change { user.reload.user_risk_state }
+
+      # Current balance is $100.01
+      create(:balance, user: user, amount_cents: 1)
+      expect { perform }.to change { user.reload.user_risk_state }.from("on_probation").to("compliant")
+      expect(user.comments.last.author_name).to eq(User::LowBalanceFraudCheck::LOW_BALANCE_FRAUD_CHECK_AUTHOR_NAME)
+    end
+
+    it "also enables refunds when marking compliant" do
+      create(:balance, user: user, amount_cents: 100_01)
+      expect { perform }.to change { user.reload.refunds_disabled? }.from(true).to(false)
+    end
+  end
+
+  context "when user is on probation for other reasons" do
+    before do
+      user.put_on_probation(author_name: "OtherReason", content: "Different probation reason")
+    end
+
+    it "does not remove probation even when balance exceeds $100" do
+      create(:balance, user: user, amount_cents: 100_01)
+      expect { perform }.not_to change { user.reload.user_risk_state }
+    end
+  end
 end
