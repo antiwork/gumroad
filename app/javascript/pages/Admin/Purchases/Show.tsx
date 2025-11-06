90fb5667e4875a71f21d3475f9df9a787d7a4a78
Maya <19721695+code-elf@users.noreply.github.com>
2025-11-06 21:53:24 +0100

    fix

--- app/javascript/pages/Admin/Purchases/Show.tsx
+++ app/javascript/pages/Admin/Purchases/Show.tsx
@@ -2,14 +2,14 @@ import { usePage } from "@inertiajs/react";
 import React from "react";
 import { cast } from "ts-safe-cast";
 
-import Product, { type Product as ProductType } from "$app/components/Admin/Products/Product";
-import Purchase, { type Purchase as PurchaseType } from "$app/components/Admin/Purchase";
-import User, { type User as UserType } from "$app/components/Admin/Users/User";
+import AdminProduct, { type Product } from "$app/components/Admin/Products/Product";
+import AdminPurchase, { type Purchase } from "$app/components/Admin/Purchases";
+import AdminUser, { type User } from "$app/components/Admin/Users/User";
 
 type AdminPurchaseProps = {
-  user: UserType;
-  product: ProductType;
-  purchase: PurchaseType;
+  user: User;
+  product: Product;
+  purchase: Purchase;
 };
 
 const AdminPurchasesShow = () => {
@@ -17,9 +17,9 @@ const AdminPurchasesShow = () => {
 
   return (
     <div className="paragraphs">
-      <Purchase purchase={purchase} />
-      <Product product={product} />
-      <User user={user} />
+      <AdminPurchase purchase={purchase} />
+      <AdminProduct product={product} />
+      <AdminUser user={user} />
     </div>
   );
 };
