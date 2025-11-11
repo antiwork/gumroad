5713f8943e33d20c15107f5cab4cbbc5c049ff2a
Maya <19721695+MayaRainer@users.noreply.github.com>
2025-11-11 14:57:28 +0100

    Update app/javascript/components/Pagination.tsx
    Co-authored-by: Jono M <reason.koan@gmail.com>

--- app/javascript/components/Pagination.tsx
+++ app/javascript/components/Pagination.tsx
@@ -56,7 +56,7 @@ export const Pagination = ({ pagination, pageDisplayCount = 10, onChangePage }: 
         <Icon name="outline-cheveron-left" />
         Previous
       </Button>
-      <menu className="!hidden flex-1 justify-center gap-2 lg:flex!">
+      <menu className="hidden! flex-1 justify-center gap-2 lg:flex!">
         {firstBoundaryPageShown ? (
           <>
             <PageNumber page={1} isCurrent={pagination.page === 1} onClick={() => onChangePage(1)} />
