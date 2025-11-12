87fd9cdeb684431897440efa30cce4f2481acea6
Paul Nizet <polonizet@gmail.com>
2025-11-12 17:01:19 +0100

    Fix(payments): Check user age for guardian info

--- app/javascript/components/server-components/Settings/PaymentsPage.tsx
+++ app/javascript/components/server-components/Settings/PaymentsPage.tsx
@@ -1186,7 +1186,9 @@ const PaymentsPage = (props: Props) => {
                 canadaBusinessTypes={props.canada_business_types}
                 states={props.states}
                 errorFieldNames={errorFieldNames}
-                isLegalGuardianInformationRequired={props.guardian_verification_state !== "not_required"}
+                isLegalGuardianInformationRequired={
+                  props.user_under_18 ? props.guardian_verification_state !== "not_required" : null
+                }
               />
             ) : (
               <StripeConnectSection
