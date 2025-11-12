d503dbfc39fef76b2d832dcfc304f9c31322d15c
Vedansh Bhartia <vedanshbhartia@gmail.com>
2025-11-12 17:27:17 -0500

    do not pass recaptcha to frontend

--- app/presenters/checkout_presenter.rb
+++ app/presenters/checkout_presenter.rb
@@ -278,7 +278,7 @@ class CheckoutPresenter
         countries: Compliance::Countries.for_select.to_h,
         us_states: STATES,
         ca_provinces: Compliance::Countries.subdivisions_for_select(Compliance::Countries::CAN.alpha2).map(&:first),
-        recaptcha_key: GlobalConfig.get("RECAPTCHA_MONEY_SITE_KEY"),
+        recaptcha_key: Rails.env.test? ? nil : GlobalConfig.get("RECAPTCHA_MONEY_SITE_KEY"),
         paypal_client_id: PAYPAL_PARTNER_CLIENT_ID,
       }
     end
