c35b166cae1fc2856a306a12856e2eed169d626e
Paul Nizet <polonizet@gmail.com>
2025-11-18 19:37:07 +0100

    Remove unused foreign key constraint
    Removes the foreign key constraint between installment_plan_snapshots and payment_options as it is no longer needed, simplifying the database schema.

--- db/schema.rb
+++ db/schema.rb
@@ -2743,5 +2743,4 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_10_144032) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "installment_plan_snapshots", "payment_options"
 end
