d227d42d8ccdffda0c4b76cda6f940139c91da11
Emmanuel Cousin <emmanuel@hey.com>
2025-11-21 15:20:37 +0100

    Update card_spec.rb

--- spec/presenters/product_presenter/card_spec.rb
+++ spec/presenters/product_presenter/card_spec.rb
@@ -41,7 +41,7 @@ describe ProductPresenter::Card do
         )
       end
 
-      it "returns the URL with the offer code when an allowed offer_code is provided" do
+      it "returns the URL with the offer code" do
         Feature.activate(:offer_codes_search)
         data = described_class.new(product:).for_web(request:, recommended_by: "discover", offer_code: "BLACKFRIDAY2025")
         expect(data[:url]).to include("code=BLACKFRIDAY2025")
@@ -49,10 +49,6 @@ describe ProductPresenter::Card do
         Feature.deactivate(:offer_codes_search)
       end
 
-      it "does not return the URL with the offer code when an disallowed offer_code is provided" do
-        data = described_class.new(product:).for_web(request:, recommended_by: "discover", offer_code: "SUMMER2025")
-        expect(data[:url]).not_to include("code=SUMMER2025")
-      end
 
       it "does not return the URL of a deleted thumbnail" do
         create(:thumbnail, product:)
