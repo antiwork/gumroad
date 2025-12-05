90a9d1d3d8e42fcf4dd213c16811adf3e4ce9d97
Nyoman Jyotisa <jyotisa1616@gmail.com>
2025-12-05 13:07:57 +0800

    update

--- app/controllers/admin/users/guids_controller.rb
+++ app/controllers/admin/users/guids_controller.rb
@@ -5,15 +5,11 @@ class Admin::Users::GuidsController < Admin::Users::BaseController
     user = User.find_by_external_id(user_param) || e404
     guids = Event.where(user_id: user.id).distinct.pluck(:browser_guid)
 
-    events_by_guid = Event.select(:user_id, :browser_guid).by_browser_guid(guids).
-                           where.not(user_id: nil).distinct.group_by(&:browser_guid)
-
-    user_ids = events_by_guid.values.flatten.map(&:user_id).uniq
-    external_ids_by_user_id = User.where(id: user_ids).pluck(:id, :external_id).to_h
-
-    guids_to_users = events_by_guid.map do |browser_guid, events|
-      { guid: browser_guid, user_external_ids: events.map { |e| external_ids_by_user_id[e.user_id] } }
-    end
+    guids_to_users = Event.select(:user_id, :browser_guid).by_browser_guid(guids)
+                          .where.not(user_id: nil).distinct
+                          .includes(:user)
+                          .group_by(&:browser_guid)
+                          .map { |browser_guid, events| { guid: browser_guid, user_external_ids: events.map(&:user_external_id) } }
 
     render json: guids_to_users
   end
