1de2d711794f25dd539e21d1a1d55862e66b7c08
adityagarud <garudaditya079@gmail.com>
2025-12-12 23:12:58 +0530

    address PR review: remove duplicate set_title before_action and revert helper_widget change

--- app/controllers/concerns/helper_widget.rb
+++ app/controllers/concerns/helper_widget.rb
@@ -30,7 +30,7 @@ module HelperWidget
 
       OpenSSL::HMAC.hexdigest(
         "sha256",
-        GlobalConfig.get("HELPER_WIDGET_SECRET") || "",
+        GlobalConfig.get("HELPER_WIDGET_SECRET"),
         message
       )
     end
