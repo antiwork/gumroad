e60f71d738751bffbfd62118397c60b682fc1869
Ershad Kunnakkadan <ershad92@gmail.com>
2025-12-15 13:19:20 -0500

    Update test

--- spec/services/user_balance_stats_service_spec.rb
+++ spec/services/user_balance_stats_service_spec.rb
@@ -5,7 +5,7 @@ require "spec_helper"
 describe UserBalanceStatsService do
   let(:user) { create(:user) }
   let(:instance) { described_class.new(user:) }
-  let(:example_values) { { foo: "bar" } }
+  let(:example_values) { { foo: "bar", nested: { key: "value" }, items: ["item1", "item2"] } }
 
   describe "#generate" do
     # We're not actually testing the values generated here.
