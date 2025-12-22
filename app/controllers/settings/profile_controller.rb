cc50d5c210cd37c87716a48fa7f3a4c3a8225a84
Maya <19721695+code-elf@users.noreply.github.com>
2025-12-22 23:56:40 +0100

    fix

--- app/controllers/settings/profile_controller.rb
+++ app/controllers/settings/profile_controller.rb
@@ -6,11 +6,11 @@ class Settings::ProfileController < Sellers::BaseController
 
   def show
     @title = "Settings"
-    @profile_presenter = ProfilePresenter.new(pundit_user:, seller: current_seller)
-    @settings_presenter = SettingsPresenter.new(pundit_user:)
+    profile_presenter = ProfilePresenter.new(pundit_user:, seller: current_seller)
+    settings_presenter = SettingsPresenter.new(pundit_user:)
 
-    render inertia: "Settings/Profile/Show", props: @settings_presenter.profile_props.merge(
-      @profile_presenter.profile_settings_props(request:)
+    render inertia: "Settings/Profile/Show", props: settings_presenter.profile_props.merge(
+      profile_presenter.profile_settings_props(request:)
     )
   end
 
