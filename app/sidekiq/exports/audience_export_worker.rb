9d9845d9c7d336ba4310f89e75495dea431b73ce
Arjun <arjunajith440@gmail.com>
2025-12-29 13:21:23 +0530

    Fix export timeout

--- app/sidekiq/exports/audience_export_worker.rb
+++ app/sidekiq/exports/audience_export_worker.rb
@@ -7,8 +7,11 @@ class Exports::AudienceExportWorker
   def perform(seller_id, recipient_id, audience_options = {})
     seller, recipient = User.find(seller_id, recipient_id)
     recipient ||= seller
+    result = nil
 
-    result = Exports::AudienceExportService.new(seller, audience_options).perform
+    WithMaxExecutionTime.timeout_queries(seconds: 1.hour) do
+      result = Exports::AudienceExportService.new(seller, audience_options).perform
+    end
 
     ContactingCreatorMailer.subscribers_data(
       recipient:,
