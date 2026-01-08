5842b0964908f989884a381fac91984f3c24cd5f
Pradumn <pradumn@tealfeed.com>
2026-01-09 04:36:57 +0530

    fix: nil in product presenter

--- app/presenters/product_presenter.rb
+++ app/presenters/product_presenter.rb
@@ -201,7 +201,7 @@ class ProductPresenter
           code: product.default_offer_code.code,
           name: product.default_offer_code.name.presence || "",
           discount: product.default_offer_code.discount,
-        } : null,
+        } : nil,
         public_files: product.alive_public_files.attached.map { PublicFilePresenter.new(public_file: _1).props },
         audio_previews_enabled: Feature.active?(:audio_previews, product.user),
         community_chat_enabled: Feature.active?(:communities, product.user) ? product.community_chat_enabled? : nil,
