872749682dad553157c6a16cd68bdcc261710ac8
Salman Najah <salmannajah123@gmail.com>
2026-01-23 00:35:33 +0530

    Rename --range-bg to --progress

--- app/javascript/components/ui/Range.tsx
+++ app/javascript/components/ui/Range.tsx
@@ -4,14 +4,14 @@ import { classNames } from "$app/utils/classNames";
 
 declare module "react" {
   export interface CSSProperties {
-    "--range-bg"?: string;
+    "--progress"?: string;
   }
 }
 
 const rangeStyles = classNames(
   "appearance-none bg-none h-[0.3125rem]",
-  "[&::-webkit-slider-runnable-track]:h-[0.3125rem] [&::-webkit-slider-runnable-track]:w-full [&::-webkit-slider-runnable-track]:cursor-pointer [&::-webkit-slider-runnable-track]:rounded-[0.25rem] [&::-webkit-slider-runnable-track]:[background:var(--range-bg)]",
-  "[&::-moz-range-track]:h-[0.3125rem] [&::-moz-range-track]:w-full [&::-moz-range-track]:cursor-pointer [&::-moz-range-track]:rounded-[0.25rem] [&::-moz-range-track]:[background:var(--range-bg)]",
+  "[&::-webkit-slider-runnable-track]:h-[0.3125rem] [&::-webkit-slider-runnable-track]:w-full [&::-webkit-slider-runnable-track]:cursor-pointer [&::-webkit-slider-runnable-track]:rounded-[0.25rem] [&::-webkit-slider-runnable-track]:[background:var(--progress)]",
+  "[&::-moz-range-track]:h-[0.3125rem] [&::-moz-range-track]:w-full [&::-moz-range-track]:cursor-pointer [&::-moz-range-track]:rounded-[0.25rem] [&::-moz-range-track]:[background:var(--progress)]",
   "[&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:border-0 [&::-webkit-slider-thumb]:size-[1em] [&::-webkit-slider-thumb]:[background:rgb(var(--color))] [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:cursor-pointer [&::-webkit-slider-thumb]:[margin-top:calc(0.5*(0.3125rem-1em))]",
   "[&::-moz-range-thumb]:appearance-none [&::-moz-range-thumb]:border-0 [&::-moz-range-thumb]:size-[1em] [&::-moz-range-thumb]:[background:rgb(var(--color))] [&::-moz-range-thumb]:rounded-full [&::-moz-range-thumb]:cursor-pointer",
 );
@@ -25,7 +25,7 @@ export const Range = React.forwardRef<
     type="range"
     className={classNames(rangeStyles, className)}
     style={{
-      "--range-bg": `linear-gradient(to right, currentColor ${progress}%, rgb(var(--color) / 0.2) ${progress}%)`,
+      "--progress": `linear-gradient(to right, currentColor ${progress}%, rgb(var(--color) / 0.2) ${progress}%)`,
       ...style,
     }}
     {...props}
