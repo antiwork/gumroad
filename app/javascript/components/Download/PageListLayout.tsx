37810f377ea9599e3f5404e9518a53e838ef1001
Mit-Patwa <mitdpatwa@gmail.com>
2026-01-22 10:49:58 +0530

    decouple scrolling

--- app/javascript/components/Download/PageListLayout.tsx
+++ app/javascript/components/Download/PageListLayout.tsx
@@ -18,7 +18,7 @@ export const PageListLayout = ({
       className,
     )}
   >
-    <div className="flex flex-col gap-4 lg:sticky lg:top-0 lg:w-80 lg:pb-8">{pageList}</div>
+    <div className="flex flex-col gap-4 overflow-y-auto lg:sticky lg:top-0 lg:w-80 lg:pb-8">{pageList}</div>
     <div className="h-0 flex-1">{children}</div>
   </div>
 );
