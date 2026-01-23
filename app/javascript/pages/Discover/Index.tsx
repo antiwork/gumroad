b239b0fca0281ddb7ca60ea197534a4c1acad233
Aditya Garud <garudaditya079@gmail.com>
2026-01-24 00:08:47 +0530

    fix

--- app/javascript/pages/Discover/Index.tsx
+++ app/javascript/pages/Discover/Index.tsx
@@ -3,8 +3,12 @@ import * as React from "react";
 
 import { DiscoverPage, type DiscoverPageProps } from "$app/components/DiscoverPage";
 
-export default function DiscoverIndex() {
+function DiscoverIndex() {
   const props = usePage<DiscoverPageProps>().props;
 
   return <DiscoverPage {...props} />;
 }
+
+DiscoverIndex.loggedInUserLayout = true;
+
+export default DiscoverIndex;
