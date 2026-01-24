4cc4c9f36a9b28de7548060dbc0623b97ddc3090
dluffy56 <bbheemaraya944@gmail.com>
2026-01-24 10:21:06 +0530

    small change

--- spec/requests/communities_inertia_spec.rb
+++ spec/requests/communities_inertia_spec.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
+
 require "spec_helper"
+require "inertia_rails/rspec"
 
 describe "Communities Inertia Endpoints", type: :request do
   include Devise::Test::IntegrationHelpers
@@ -14,24 +16,27 @@ describe "Communities Inertia Endpoints", type: :request do
     sign_in seller
   end
 
+
   it "renders Inertia component for /communities" do
     get "/communities"
     expect(response).to be_successful
     expect(response.headers["Vary"]).to include("Accept")
-    expect(response.body).to include("Communities/Index")
+    expect(inertia.component).to eq("Communities/Index")
   end
+
 
   it "renders Inertia component for /communities/:id" do
     get "/communities/#{community.external_id}"
     expect(response).to be_successful
     expect(response.headers["Vary"]).to include("Accept")
-    expect(response.body).to include("Communities/Show")
+    expect(inertia.component).to eq("Communities/Show")
   end
+
 
   it "renders Inertia component for /communities/:seller_id/:community_iid" do
     get "/communities/#{seller.id}/#{community.external_id}"
     expect(response).to be_successful
     expect(response.headers["Vary"]).to include("Accept")
-    expect(response.body).to include("Communities/Show")
+    expect(inertia.component).to eq("Communities/Show")
   end
 end
