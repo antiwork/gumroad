67335ed7f78cac753e6198705870f4af52578e38
avi-474 <avichoudhary474@gmail.com>
2026-01-27 15:47:26 +0530

    Migrate followers cancellation page to Inertia and refine UI

--- spec/controllers/followers_controller_spec.rb
+++ spec/controllers/followers_controller_spec.rb
@@ -225,11 +225,12 @@ describe FollowersController, inertia: true do
     end
 
     describe "GET cancel" do
-      it "cancels the follow" do
+      it "cancels the follow and renders the Inertia page" do
         follower = create(:follower)
         expect { get :cancel, params: { id: follower.external_id } }.to change {
           follower.reload.deleted?
         }.from(false).to(true)
+        expect(inertia.component).to eq("Followers/Cancel")
       end
 
       it "returns 404 when follower is invalid" do
