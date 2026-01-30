648785beb68216d77e58e0a6956b9ebf15b082a9
abhi <abhiifour@gmail.com>
2026-01-30 16:10:31 +0530

    fix: popover anchor

--- app/javascript/components/Popover.tsx
+++ app/javascript/components/Popover.tsx
@@ -5,7 +5,18 @@ import { classNames } from "$app/utils/classNames";
 
 export const Popover = PopoverPrimitive.Root;
 export const PopoverClose = PopoverPrimitive.Close;
-export const PopoverAnchor = PopoverPrimitive.Anchor;
+
+export const PopoverAnchor = React.forwardRef<
+  React.ElementRef<typeof PopoverPrimitive.Anchor>,
+  React.ComponentPropsWithoutRef<typeof PopoverPrimitive.Anchor>
+>(({ className, ...props }, ref) => (
+  <PopoverPrimitive.Anchor
+    ref={ref}
+    className={classNames("grid", className)}
+    {...props}
+  />
+));
+PopoverAnchor.displayName = PopoverPrimitive.Anchor.displayName;
 
 export const PopoverTrigger = React.forwardRef<
   React.ElementRef<typeof PopoverPrimitive.Trigger>,
