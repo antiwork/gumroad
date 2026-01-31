b420d83954e92713499346e0835285d1d4fcd832
mxrcabrera <mxrcabrera@gmail.com>
2026-01-31 05:57:09 +0000

    feat: Migrate Coffee Page to Inertia (#3144)

--- app/javascript/components/Profile/Layout.tsx
+++ app/javascript/components/Profile/Layout.tsx
@@ -36,7 +36,7 @@ export const Layout = ({ creatorProfile, hideFollowForm, children }: LayoutProps
     ) : null;
 
   return (
-    <div className="flex min-h-full flex-col">
+    <div className="flex min-h-screen flex-col">
       <header className="z-20 border-border bg-background text-lg lg:border-b lg:px-4 lg:py-6">
         <div className="mx-auto flex max-w-6xl flex-wrap lg:flex-nowrap lg:items-center lg:gap-6">
           <div className="relative flex grow items-center gap-3 border-b border-border px-4 py-8 lg:flex-1 lg:border-0 lg:p-0">
@@ -66,7 +66,7 @@ export const Layout = ({ creatorProfile, hideFollowForm, children }: LayoutProps
           {isDesktop ? headerButtons : null}
         </div>
       </header>
-      <main className="flex-1">
+      <main className="flex flex-1 flex-col">
         {children}
         <PoweredByFooter className="mx-auto w-full max-w-6xl lg:py-6 lg:text-left" />
       </main>
