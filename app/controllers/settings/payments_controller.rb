b076ea19dd7131cec76d069e654b8195605868ab
Gianfranco Piana <52470719+gianfrancopiana@users.noreply.github.com>
2026-02-06 11:23:35 -0300

    Fix postal code error handling rescue block
    Co-authored-by: Emmanuel Cousin <EmCousin@users.noreply.github.com>

--- app/controllers/settings/payments_controller.rb
+++ app/controllers/settings/payments_controller.rb
@@ -83,14 +83,12 @@ class Settings::PaymentsController < Settings::BaseController
         StripeMerchantAccountManager.create_account(current_seller, passphrase: GlobalConfig.get("STRONGBOX_GENERAL_PASSWORD"))
       rescue Stripe::InvalidRequestError => e
         if e.code == "postal_code_invalid"
-          return redirect_with_error("The postal code you entered is not recognized. Please double-check your postal code and try again.")
-        end
-        return redirect_with_error(e.try(:message) || "Something went wrong.")
-      rescue => e
-        if e.try(:code) == "postal_code_invalid"
           country = current_seller.fetch_or_build_user_compliance_info.legal_entity_country
-          return redirect_with_error("The postal code you entered is not valid for #{country}.")
+          redirect_with_error("The postal code you entered is not valid for #{country}.")
+        else
+          redirect_with_error(e.try(:message) || "Something went wrong.")
         end
+        return
         return redirect_with_error(e.try(:message) || "Something went wrong.")
       end
     end
