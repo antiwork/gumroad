d1be06a790d6f2c12a8a1de77d973e723e297e3e
TenzDelek <tibetdelek@gmail.com>
2026-02-06 21:48:11 +0530

    overflow fix

--- app/javascript/components/LoadingSkeleton/index.tsx
+++ app/javascript/components/LoadingSkeleton/index.tsx
@@ -5,13 +5,13 @@ import { PageHeader } from "$app/components/ui/PageHeader";
 
 function LoadingSkeleton() {
   return (
-    <div className="flex-1">
+    <div className="flex-1 overflow-y-auto">
       <PageHeader className="border-none" title={<Skeleton className="h-12 w-56" />} />
-      <section className="h-full space-y-4 p-4 md:p-8">
-        <Skeleton className="h-1/5 w-full" />
-        <Skeleton className="h-1/5 w-full" />
-        <Skeleton className="h-1/5 w-full" />
-        <Skeleton className="h-1/5 w-full" />
+      <section className="space-y-4 p-4 md:p-8">
+        <Skeleton className="h-32 w-full" />
+        <Skeleton className="h-32 w-full" />
+        <Skeleton className="h-32 w-full" />
+        <Skeleton className="h-32 w-full" />
       </section>
     </div>
   );
