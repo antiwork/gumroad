662ff700fd42d506dcc89bdee4ecfd46995ec80e
TenzDelek <tibetdelek@gmail.com>
2026-02-11 19:34:13 +0530

    update

--- app/javascript/components/LoadingSkeleton/index.tsx
+++ app/javascript/components/LoadingSkeleton/index.tsx
@@ -5,13 +5,13 @@ import { PageHeader } from "$app/components/ui/PageHeader";
 
 function LoadingSkeleton() {
   return (
-    <div className="flex-1 overflow-y-auto">
+    <div className="flex flex-1 flex-col overflow-y-auto">
       <PageHeader className="border-none" title={<Skeleton className="h-12 w-56" />} />
-      <section className="space-y-4 p-4 md:p-8">
-        <Skeleton className="h-32 w-full" />
-        <Skeleton className="h-32 w-full" />
-        <Skeleton className="h-32 w-full" />
-        <Skeleton className="h-32 w-full" />
+      <section className="flex flex-1 flex-col gap-4 p-4 md:p-8">
+        <Skeleton className="w-full flex-1" />
+        <Skeleton className="w-full flex-1" />
+        <Skeleton className="w-full flex-1" />
+        <Skeleton className="w-full flex-1" />
       </section>
     </div>
   );
