532c28353451415bf67414bf84bb3bc5b06098e8
Hanaffi <ahmedellord56@gmail.com>
2026-02-12 21:23:35 +0200

    respect useragent locale

--- app/javascript/components/BundleEdit/ContentTab/BundleProductSelector.tsx
+++ app/javascript/components/BundleEdit/ContentTab/BundleProductSelector.tsx
@@ -10,6 +10,7 @@ import {
   CartItemFooter,
 } from "$app/components/CartItemList";
 import { Thumbnail } from "$app/components/Product/Thumbnail";
+import { useUserAgentInfo } from "$app/components/UserAgent";
 
 export const BundleProductSelector = ({
   bundleProduct,
@@ -19,32 +20,42 @@ export const BundleProductSelector = ({
   bundleProduct: BundleProduct;
   selected?: boolean;
   onToggle: () => void;
-}) => (
-  <CartItem>
-    <CartItemMedia className="sm:w-24">
-      <Thumbnail url={bundleProduct.thumbnail_url} nativeType={bundleProduct.native_type} className="size-full" />
-    </CartItemMedia>
-    <CartItemMain>
-      <div>
-        <CartItemTitle>{bundleProduct.name}</CartItemTitle>
-        <a href={bundleProduct.url} target="_blank" rel="noopener noreferrer nofollow">
-          {bundleProduct.url}
-        </a>
-      </div>
-      <CartItemFooter>
-        <ul className="inline">
-          <li>{new Date(bundleProduct.creation_date).toLocaleDateString()}</li>
-          {bundleProduct.variants ? (
+}) => {
+  const userAgentInfo = useUserAgentInfo();
+
+  return (
+    <CartItem>
+      <CartItemMedia className="sm:w-24">
+        <Thumbnail url={bundleProduct.thumbnail_url} nativeType={bundleProduct.native_type} className="size-full" />
+      </CartItemMedia>
+      <CartItemMain>
+        <div>
+          <CartItemTitle>{bundleProduct.name}</CartItemTitle>
+          <a href={bundleProduct.url} target="_blank" rel="noopener noreferrer nofollow">
+            {bundleProduct.url}
+          </a>
+        </div>
+        <CartItemFooter>
+          <ul className="inline">
             <li>
-              {bundleProduct.variants.list.length} {bundleProduct.variants.list.length === 1 ? "version" : "versions"}{" "}
-              available
+              {new Date(bundleProduct.creation_date).toLocaleDateString(userAgentInfo.locale, {
+                month: "numeric",
+                day: "numeric",
+                year: "numeric",
+              })}
             </li>
-          ) : null}
-        </ul>
-      </CartItemFooter>
-    </CartItemMain>
-    <CartItemEnd className="justify-center">
-      <input type="checkbox" aria-label={bundleProduct.name} checked={!!selected} onChange={onToggle} />
-    </CartItemEnd>
-  </CartItem>
-);
+            {bundleProduct.variants ? (
+              <li>
+                {bundleProduct.variants.list.length} {bundleProduct.variants.list.length === 1 ? "version" : "versions"}{" "}
+                available
+              </li>
+            ) : null}
+          </ul>
+        </CartItemFooter>
+      </CartItemMain>
+      <CartItemEnd className="justify-center">
+        <input type="checkbox" aria-label={bundleProduct.name} checked={!!selected} onChange={onToggle} />
+      </CartItemEnd>
+    </CartItem>
+  );
+};
