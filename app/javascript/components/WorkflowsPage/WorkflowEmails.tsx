c6e50df4e6d8774c17ca78b4a1d85cc13f51f524
alina-alyy <Alina.ali2712@gmail.com>
2026-02-19 13:32:10 +0500

    fix: restore transition

--- app/javascript/components/WorkflowsPage/WorkflowEmails.tsx
+++ app/javascript/components/WorkflowsPage/WorkflowEmails.tsx
@@ -557,11 +557,12 @@ const EmailRow = ({
 
 const EmailPreview = ({
   email,
+  isEditing,
   workflowTrigger,
   gumroadAddress,
 }: {
   email: EmailFormState;
-  isEditing?: boolean;
+  isEditing: boolean;
   workflowTrigger: WorkflowTrigger;
   gumroadAddress: string;
 }) => {
@@ -575,6 +576,10 @@ const EmailPreview = ({
   });
   const selfRef = React.useRef<HTMLDivElement>(null);
   const emailFiles = useFiles((files) => files.filter(({ email_id }) => email_id === email.id));
+
+  React.useEffect(() => {
+    if (isEditing) setTimeout(() => selfRef.current?.scrollIntoView({ behavior: "smooth" }), 500);
+  }, [isEditing]);
 
   return (
     <section className="flex flex-col gap-4" ref={selfRef}>
