21536fa2c38e94c8200d88742bc73a0be0b17e2d
Gianfranco Piana <52470719+gianfrancopiana@users.noreply.github.com>
2026-02-22 05:55:23 -0500

    Optimize global sales tax summary report job

--- app/sidekiq/create_global_sales_tax_summary_report_job.rb
+++ app/sidekiq/create_global_sales_tax_summary_report_job.rb
@@ -15,21 +15,32 @@ class CreateGlobalSalesTaxSummaryReportJob
 
     timeout_seconds = ($redis.get(RedisKey.create_global_sales_tax_summary_report_job_max_execution_time_seconds) || 1.hour).to_i
     WithMaxExecutionTime.timeout_queries(seconds: timeout_seconds) do
-      Purchase.successful
+      purchases_scope = Purchase.successful
         .not_fully_refunded
         .not_chargedback_or_chargedback_reversed
         .where.not(stripe_transaction_id: nil)
         .where("gumroad_tax_cents > 0")
         .where("purchases.created_at BETWEEN ? AND ?", start_date, end_date)
         .where(charge_processor_id: [nil, *ChargeProcessor.charge_processor_ids])
+
+      refund_sums = Refund
+        .where(purchase_id: purchases_scope.where(stripe_partially_refunded: true).select(:id))
+        .group(:purchase_id)
+        .pluck(:purchase_id, Arel.sql("SUM(refunds.total_transaction_cents)"), Arel.sql("SUM(refunds.gumroad_tax_cents)"))
+        .to_h { |pid, total, tax| [pid, { total: total.to_i, tax: tax.to_i }] }
+
+      purchases_scope
+        .select(:id, :country, :ip_country, :state, :ip_state, :zip_code, :ip_address,
+                :total_transaction_cents, :gumroad_tax_cents, :stripe_partially_refunded)
         .find_each do |purchase|
         country_name = resolve_country_name(purchase)
         state_code = resolve_state(purchase, country_name)
+        refund = refund_sums[purchase.id] if purchase.stripe_partially_refunded?
 
-        key = [country_name, state_code]
-        aggregation[key][:gmv_cents] += purchase.total_cents_net_of_refunds
-        aggregation[key][:order_count] += 1
-        aggregation[key][:tax_collected_cents] += purchase.gumroad_tax_cents_net_of_refunds
+        bucket = aggregation[[country_name, state_code]]
+        bucket[:gmv_cents] += net_cents(purchase.total_transaction_cents, refund&.dig(:total))
+        bucket[:order_count] += 1
+        bucket[:tax_collected_cents] += net_cents(purchase.gumroad_tax_cents, refund&.dig(:tax))
       end
     end
 
@@ -39,10 +50,7 @@ class CreateGlobalSalesTaxSummaryReportJob
       temp_file = Tempfile.new
       temp_file.write(row_headers.to_csv)
 
-      aggregation.keys.sort.each do |key|
-        country_name, state_code = key
-        data = aggregation[key]
-
+      aggregation.sort.each do |(country_name, state_code), data|
         temp_file.write([
           country_name,
           state_code,
@@ -73,8 +81,14 @@ class CreateGlobalSalesTaxSummaryReportJob
       raw_name = purchase.country.presence || purchase.ip_country.presence
       return "Unknown" if raw_name.blank?
 
-      country = Compliance::Countries.find_by_name(raw_name)
-      country&.common_name || raw_name
+      normalized_country_names[raw_name]
+    end
+
+    def normalized_country_names
+      @normalized_country_names ||= Hash.new do |hash, raw_name|
+        country = Compliance::Countries.find_by_name(raw_name)
+        hash[raw_name] = country&.common_name || raw_name
+      end
     end
 
     def resolve_state(purchase, country_name)
@@ -119,6 +133,14 @@ class CreateGlobalSalesTaxSummaryReportJob
         raw_state
       else
         ""
+      end
+    end
+
+    def net_cents(gross_cents, refunded_cents)
+      if refunded_cents
+        [gross_cents - refunded_cents, 0].max
+      else
+        gross_cents
       end
     end
 end
