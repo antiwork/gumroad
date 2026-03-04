1782e4b3b2624fe914043e6ede63263545a05705
Salman Najah <salmannajah123@gmail.com>
2026-03-04 22:01:34 +0530

    Forms migration part 4

--- app/javascript/components/ui/Select.tsx
+++ app/javascript/components/ui/Select.tsx
@@ -14,15 +14,14 @@ export const Select = React.forwardRef<
 
   return (
     <div className={classNames("relative inline-grid", wrapperClassName)}>
-      {/* bg-none! should be removed after complete forms migration */}
       <select
         ref={ref}
-        className={classNames(baseInputStyles, "appearance-none bg-none! pr-10", stateBorderStyles[state], className)}
+        className={classNames(baseInputStyles, "appearance-none bg-none pr-10", stateBorderStyles[state], className)}
         {...props}
       >
         {children}
       </select>
-      <ChevronDown className="pointer-events-none absolute top-1/2 right-4 size-5 -translate-y-1/2 text-muted" />
+      <ChevronDown className="pointer-events-none absolute top-1/2 right-4 size-5 -translate-y-1/2" />
     </div>
   );
 });
