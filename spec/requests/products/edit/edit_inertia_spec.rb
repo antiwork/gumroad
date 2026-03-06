792594b9df1163f364dcbeb76b52330f98cb5ed7
Gianfranco Piana <52470719+gianfrancopiana@users.noreply.github.com>
2026-03-06 14:15:21 -0300

    Swap product edit to Inertia and remove React on Rails artifacts

--- spec/requests/products/edit/edit_inertia_spec.rb
+++ spec/requests/products/edit/edit_inertia_spec.rb
@@ -2,7 +2,7 @@
 
 require "spec_helper"
 
-describe("Product Edit New (Inertia) Scenario", type: :system, js: true) do
+describe("Product Edit (Inertia) Scenario", type: :system, js: true) do
   include ProductEditPageHelpers
 
   let(:seller) { create(:named_seller) }
@@ -13,36 +13,36 @@ describe("Product Edit New (Inertia) Scenario", type: :system, js: true) do
   end
 
   it "loads the product edit page via Inertia" do
-    visit edit_link_new_path(product.unique_permalink)
+    visit edit_link_path(product.unique_permalink)
     expect(page).to have_text(product.name)
   end
 
   it "navigates between tabs" do
-    visit edit_link_new_path(product.unique_permalink)
+    visit edit_link_path(product.unique_permalink)
     expect(page).to have_text(product.name)
 
     click_on "Content"
-    expect(page).to have_current_path(%r{/edit_new/content})
+    expect(page).to have_current_path(%r{/edit/content})
 
     click_on "Receipt"
-    expect(page).to have_current_path(%r{/edit_new/receipt})
+    expect(page).to have_current_path(%r{/edit/receipt})
 
     click_on "Product"
-    expect(page).to have_current_path(%r{/edit_new\z})
+    expect(page).to have_current_path(%r{/edit\z})
   end
 
   it "loads sub-routes directly (browser refresh)" do
-    visit "/products/#{product.unique_permalink}/edit_new/content"
-    expect(page).to have_current_path(%r{/edit_new/content})
+    visit "/products/#{product.unique_permalink}/edit/content"
+    expect(page).to have_current_path(%r{/edit/content})
     expect(page).to have_text(product.name)
 
-    visit "/products/#{product.unique_permalink}/edit_new/receipt"
-    expect(page).to have_current_path(%r{/edit_new/receipt})
+    visit "/products/#{product.unique_permalink}/edit/receipt"
+    expect(page).to have_current_path(%r{/edit/receipt})
     expect(page).to have_text(product.name)
   end
 
   it "saves changes" do
-    visit edit_link_new_path(product.unique_permalink)
+    visit edit_link_path(product.unique_permalink)
     expect(page).to have_text(product.name)
     save_change
   end
