8d29833082d4456a980170ea7d8dca42454c7074
Salman Najah <salmannajah123@gmail.com>
2026-04-01 22:05:46 +0530

    Fix

--- app/javascript/components/Product/ShareSection.tsx
+++ app/javascript/components/Product/ShareSection.tsx
@@ -122,7 +122,7 @@ export const ShareSection = ({
               <div
                 {...props}
                 inert={isSelectionInWishlist(wishlist)}
-                className={classNames(props.className, isSelectionInWishlist(wishlist) && "opacity-30")}
+                className={classNames(props.className, isSelectionInWishlist(wishlist) ? "opacity-30" : undefined)}
                 onClick={(e) => {
                   props.onClick?.(e);
                   void addProduct(Promise.resolve({ newlyCreated: false, wishlist }));
