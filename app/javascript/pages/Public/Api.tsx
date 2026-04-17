690937abca281334c64b27706b384027b2b8a098
Gianfranco Piana <gianfrancopiana@users.noreply.github.com>
2026-04-17 16:12:09 -0300

    Add POST /v2/files/abort for multipart uploads

--- app/javascript/pages/Public/Api.tsx
+++ app/javascript/pages/Public/Api.tsx
@@ -8,7 +8,7 @@ import {
   UpdateCustomField,
   DeleteCustomField,
 } from "$app/components/ApiDocumentation/Endpoints/CustomFields";
-import { FilesOverview, PresignFile, CompleteFile, AttachFile } from "$app/components/ApiDocumentation/Endpoints/Files";
+import { FilesOverview, PresignFile, CompleteFile, AbortFile, AttachFile } from "$app/components/ApiDocumentation/Endpoints/Files";
 import {
   VerifyLicense,
   EnableLicense,
@@ -107,6 +107,7 @@ export default function Api() {
                 <FilesOverview />
                 <PresignFile />
                 <CompleteFile />
+                <AbortFile />
                 <AttachFile />
               </ApiResource>
 
