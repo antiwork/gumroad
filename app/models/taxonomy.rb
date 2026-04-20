f2b97b76494ffdaf8f7dcc81fb23d3b67d449657
gumclaw <gumclaw@gumroad.com>
2026-04-20 17:12:30 -0400

    Fix race condition in Taxonomy find_or_create_by causing RecordNotUnique

--- app/models/taxonomy.rb
+++ app/models/taxonomy.rb
@@ -7,4 +7,18 @@ class Taxonomy < ApplicationRecord
   has_one :taxonomy_stat, dependent: :destroy
 
   validates :slug, presence: true, uniqueness: { scope: :parent_id }
+
+  def self.find_or_create_by!(attributes, &block)
+    super
+  rescue ActiveRecord::RecordNotUnique
+    ActiveRecord::Base.connection.stick_to_primary!
+    find_by!(attributes)
+  end
+
+  def self.find_or_create_by(attributes, &block)
+    super
+  rescue ActiveRecord::RecordNotUnique
+    ActiveRecord::Base.connection.stick_to_primary!
+    find_by(attributes)
+  end
 end
