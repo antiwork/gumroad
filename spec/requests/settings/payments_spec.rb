45dff7cfa01b505d501d40e164c9c28fb695fddf
gumclaw <gumclaw@gumroad.com>
2026-04-21 16:06:02 -0400

    Fix payments settings PO box test label

--- spec/requests/settings/payments_spec.rb
+++ spec/requests/settings/payments_spec.rb
@@ -689,7 +689,7 @@ describe("Payments Settings Scenario", type: :system, js: true) do
           expect(page).to have_alert(text: "Thanks! You're all set.")
           sleep 0.5 # Since the previous Alerts takes time to disappear, checking alert returns early before the api call is complete
         end.to change { @user.alive_user_compliance_info.reload.business_street_address }.to("123 North street")
-        fill_in "Street address", with: "po box 123 smith street"
+        fill_in "Address", with: "po box 123 smith street"
         expect do
           click_on "Update settings"
           expect(page).to have_status(text: "We require a valid physical US address. We cannot accept a P.O. Box as a valid address.")
