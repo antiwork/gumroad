4b3fb3384cec4aed82a98e728d5c5d978db0eeca
Gumclaw <gumclaw@gumroad.com>
2026-05-08 13:17:54 -0400

    Remove support controller, routes, and all dead support components
    - Delete SupportController and its spec (no user-facing support routes needed)
    - Delete Support/Index page
    - Delete SupportPortal, ConversationDetail, ConversationList,
      SupportSlaMessage, UnreadTicketsBadge (all unused)
    - Remove support resource from routes.rb
    
    Requested by: Sahil
    Context: 'we can remove app/controllers/support_controller.rb
    entirely there is no need'

--- app/javascript/components/support/SupportPortal.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-import { SupportHeader } from "$app/components/server-components/support/Header";
-
-export default function SupportPortal() {
-  return <SupportHeader />;
-}
--- config/routes.rb
+++ config/routes.rb
@@ -965,7 +965,6 @@ Rails.application.routes.draw do
 
     resources :reviews, only: [:index]
 
-    resources :support, only: [:index]
 
     # url redirects
     get "/r/:id/expired", to: "url_redirects#expired", as: :url_redirect_expired_page
