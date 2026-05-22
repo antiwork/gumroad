c07d73f37cb6af52b52e67e28cf3ff23b99a8251
Gianfranco Piana <gianfrancopiana@users.noreply.github.com>
2026-05-22 12:06:07 -0300

    Add OAuth direct upload endpoint

---
+++ app/controllers/api/v2/direct_uploads_controller.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+class Api::V2::DirectUploadsController < Api::V2::BaseController
+  include ActiveStorage::SetCurrent
+
+  before_action { doorkeeper_authorize! :edit_products }
+
+  def create
+    blob = ActiveStorage::Blob.create_before_direct_upload!(**blob_args)
+
+    render json: direct_upload_json(blob)
+  end
+
+  private
+    def blob_args
+      params.require(:blob).permit(:filename, :byte_size, :checksum, :content_type, metadata: {}).to_h.symbolize_keys
+    end
+
+    def direct_upload_json(blob)
+      blob.as_json(root: false, methods: :signed_id).merge(
+        direct_upload: {
+          url: blob.service_url_for_direct_upload,
+          headers: blob.service_headers_for_direct_upload
+        }
+      )
+    end
+end
---
+++ spec/controllers/api/v2/direct_uploads_controller_spec.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+require "digest/md5"
+require "shared_examples/authorized_oauth_v1_api_method"
+
+describe Api::V2::DirectUploadsController do
+  before do
+    @user = create(:user)
+    @app = create(:oauth_application, owner: create(:user))
+  end
+
+  describe "POST 'create'" do
+    before do
+      @action = :create
+      @params = {
+        blob: {
+          filename: "cover.png",
+          byte_size: 1024,
+          checksum: Digest::MD5.base64digest("cover image"),
+          content_type: "image/png"
+        }
+      }
+    end
+
+    it_behaves_like "authorized oauth v1 api method"
+    it_behaves_like "authorized oauth v1 api method only for edit_products scope"
+
+    describe "when logged in with edit_products scope" do
+      before do
+        @token = create("doorkeeper/access_token", application: @app, resource_owner_id: @user.id, scopes: "edit_products")
+        @params.merge!(access_token: @token.token)
+      end
+
+      it "creates a blob for direct upload and returns the signed blob token" do
+        expect do
+          post @action, params: @params
+        end.to change { ActiveStorage::Blob.count }.by(1)
+
+        expect(response).to be_successful
+        body = response.parsed_body
+        blob = ActiveStorage::Blob.last
+        expect(body["signed_id"]).to eq(blob.signed_id)
+        expect(body["filename"]).to eq("cover.png")
+        expect(body["byte_size"]).to eq(1024)
+        expect(body["checksum"]).to eq(Digest::MD5.base64digest("cover image"))
+        expect(body["content_type"]).to eq("image/png")
+        expect(body["direct_upload"]["url"]).to be_present
+        expect(body["direct_upload"]["headers"]).to be_present
+      end
+    end
+
+    it "grants access with the account scope" do
+      token = create("doorkeeper/access_token", application: @app, resource_owner_id: @user.id, scopes: "account")
+      post @action, params: @params.merge(access_token: token.token)
+      expect(response).to be_successful
+    end
+  end
+end
