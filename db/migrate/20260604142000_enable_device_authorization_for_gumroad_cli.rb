03ac0d9642263abf293163793ba1fce63126bdda
Gianfranco Piana <gianfrancopiana@users.noreply.github.com>
2026-06-05 16:04:37 -0300

    Enable CLI device authorization

---
+++ db/migrate/20260604142000_enable_device_authorization_for_gumroad_cli.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+class EnableDeviceAuthorizationForGumroadCli < ActiveRecord::Migration[7.1]
+  CLI_CLIENT_ID = "oljO5HmcOWvCZ5wbitpXPXk3u0LjAb5GdAEBBU5hwKA"
+
+  def up
+    oauth_applications
+      .where(uid: CLI_CLIENT_ID)
+      .update_all(device_authorization_enabled: true, updated_at: Time.current)
+  end
+
+  def down
+    oauth_applications
+      .where(uid: CLI_CLIENT_ID)
+      .update_all(device_authorization_enabled: false, updated_at: Time.current)
+  end
+
+  private
+    def oauth_applications
+      Class.new(ActiveRecord::Base) do
+        self.table_name = "oauth_applications"
+      end
+    end
+end
