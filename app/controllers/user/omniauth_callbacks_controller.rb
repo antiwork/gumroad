3fcd5490e2f28d6f5e4c1e033a3c682c01ea632f
Razvan Marescu <razvan@marescu.net>
2026-07-22 08:32:22 -0700

    Cover seller Stripe connect flow
    Ensure account switching is not blocked when only the acting admin has a Stripe account, and name the selected seller in the blocked-state alert.

--- app/controllers/user/omniauth_callbacks_controller.rb
+++ app/controllers/user/omniauth_callbacks_controller.rb
@@ -61,7 +61,7 @@ class User::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     Rails.logger.info("Stripe Connect referer: #{referer}, parameters: #{LogRedactor.redact(auth)}")
 
     if current_seller&.stripe_connect_account.present?
-      flash[:alert] = "You already have another Stripe account connected with your Gumroad account."
+      flash[:alert] = "This seller already has another Stripe account connected with Gumroad."
       return safe_redirect_to settings_payments_path
     end
 
