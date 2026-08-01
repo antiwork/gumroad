a25ebee821e24719091c30dfa610078fe69745f1
gumclaw <gumclaw@gumroad.com>
2026-08-01 05:52:45 -0400

    Make membership price notices durable (#6733)

--- app/sidekiq/send_memberships_price_update_emails_job.rb
+++ app/sidekiq/send_memberships_price_update_emails_job.rb
@@ -9,14 +9,27 @@ class SendMembershipsPriceUpdateEmailsJob
       .applicable_for_product_price_change_as_of(7.days.from_now.to_date)
       .where(notified_subscriber_at: nil)
       .find_each do |subscription_plan_change|
-        subscription = subscription_plan_change.subscription
-        next if !subscription.alive? || subscription.pending_cancellation?
+        next unless subscription_plan_change.price_change_notification_recipient_eligible?
 
-        subscription_plan_change.update!(notified_subscriber_at: Time.current)
-        CustomerLowPriorityMailer.subscription_price_change_notification(
-          subscription_id: subscription.id,
-          new_price: subscription_plan_change.perceived_price_cents,
-        ).deliver_later(queue: "low")
+        claim_id = subscription_plan_change.claim_price_change_notification
+        next unless claim_id
+
+        begin
+          job = SendMembershipPriceUpdateEmailJob.perform_later(subscription_plan_change.id, claim_id)
+          raise "Membership price update notification was not enqueued" unless job
+        rescue
+          begin
+            subscription_plan_change.release_price_change_notification_claim(claim_id)
+          rescue => release_error
+            Rails.logger.error(
+              "SendMembershipsPriceUpdateEmailsJob could not release the notification claim for " \
+              "subscription plan change #{subscription_plan_change.id}: " \
+              "#{release_error.class}: #{release_error.message}"
+            )
+            ErrorNotifier.notify(release_error)
+          end
+          raise
+        end
       end
   end
 end
