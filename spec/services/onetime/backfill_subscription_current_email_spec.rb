d8656cee22a07d28b25221e4b440fb82ff0edba1
gumclaw <gumclaw@gumroad.com>
2026-08-03 09:09:18 -0400

    Drop a trailing blank line rubocop flagged

--- spec/services/onetime/backfill_subscription_current_email_spec.rb
+++ spec/services/onetime/backfill_subscription_current_email_spec.rb
@@ -103,5 +103,4 @@ RSpec.describe Onetime::BackfillSubscriptionCurrentEmail do
     expect(Sidekiq::Client).not_to have_received(:push_bulk)
     expect(ReplicaLagWatcher).not_to have_received(:watch)
   end
-
 end
