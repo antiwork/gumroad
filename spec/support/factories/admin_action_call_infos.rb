b93543fa743cb589791782b5bfd6b8f31c507934
gumclaw <gumclaw@gumroad.com>
2026-08-05 04:20:11 -0400

    Delete the admin web UI (keep the internal admin API) (#7069)

--- spec/support/factories/admin_action_call_infos.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :admin_action_call_info do
-    controller_name { "Admin::UsersController" }
-    action_name { "show" }
-    call_count { 0 }
-  end
-end
