f2ce93313892bc914b33815061a8a25839a0188b
gumclaw <gumclaw@gumroad.com>
2026-08-09 05:38:11 -0400

    Cap the "payout in processing" safeguard age (gp#1918) (#7140)

--- app/business/payments/payouts/processor/stripe/stripe_payout_processor.rb
+++ app/business/payments/payouts/processor/stripe/stripe_payout_processor.rb
@@ -31,8 +31,9 @@ class StripePayoutProcessor
   def self.is_user_payable(user, amount_payable_usd_cents, add_comment: false, from_admin: false, payout_type: Payouts::PAYOUT_TYPE_STANDARD)
     payout_date = Time.current.to_fs(:formatted_date_full_month)
 
-    # If a user's previous payment is still processing, don't allow for new payments.
-    processing_payment_ids = user.payments.processing.ids
+    # If a user's previous payment is still processing (and not stuck, see
+    # Payment::STUCK_PROCESSING_AGE), don't allow for new payments.
+    processing_payment_ids = user.payments.blocking_next_payout.ids
     if processing_payment_ids.any?
       user.add_payout_note(content: "Payout on #{payout_date} was skipped because there was already a payout in processing.") if add_comment
       return false
