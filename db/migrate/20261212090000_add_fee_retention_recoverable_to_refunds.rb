4a1d7d7509b41fe1a6f509fee0fc8fcb4d4b0df7
gumclaw <gumclaw@gumroad.com>
2026-09-09 06:54:41 -0400

    Use bulk change_table for fee-retention recoverable column
    Rails/BulkChangeTable requires combining the column add and index into one ALTER.

--- db/migrate/20261212090000_add_fee_retention_recoverable_to_refunds.rb
+++ db/migrate/20261212090000_add_fee_retention_recoverable_to_refunds.rb
@@ -2,7 +2,9 @@
 
 class AddFeeRetentionRecoverableToRefunds < ActiveRecord::Migration[7.1]
   def change
-    add_column :refunds, :fee_retention_recoverable, :boolean, default: false, null: false
-    add_index :refunds, :fee_retention_recoverable, name: "index_refunds_on_fee_retention_recoverable"
+    change_table :refunds, bulk: true do |t|
+      t.boolean :fee_retention_recoverable, default: false, null: false
+      t.index :fee_retention_recoverable, name: "index_refunds_on_fee_retention_recoverable"
+    end
   end
 end
