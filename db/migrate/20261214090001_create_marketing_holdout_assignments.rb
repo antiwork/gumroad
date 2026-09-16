e459a86891f4384972211887b535f2a3d460fddf
gumclaw <gumclaw@gumroad.com>
2026-09-16 17:59:26 -0400

    Allow holdout migration to follow its earlier local version

--- db/migrate/20261214090001_create_marketing_holdout_assignments.rb
+++ db/migrate/20261214090001_create_marketing_holdout_assignments.rb
@@ -2,7 +2,7 @@
 
 class CreateMarketingHoldoutAssignments < ActiveRecord::Migration[7.1]
   def change
-    create_table :marketing_holdout_assignments do |t|
+    create_table :marketing_holdout_assignments, if_not_exists: true do |t|
       t.references :user, null: false, index: { unique: true }
       t.string :prior_sales_bucket, null: false
       t.boolean :marketing_holdout, null: false
