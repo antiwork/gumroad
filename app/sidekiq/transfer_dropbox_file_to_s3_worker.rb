e157d879db66fb6b5be7c7f5e7bc87736b107966
gumclaw <gumclaw@gumroad.com>
2026-09-22 02:20:01 -0400

    Read Dropbox import state from primary to tolerate replica lag

--- app/sidekiq/transfer_dropbox_file_to_s3_worker.rb
+++ app/sidekiq/transfer_dropbox_file_to_s3_worker.rb
@@ -5,7 +5,10 @@ class TransferDropboxFileToS3Worker
   sidekiq_options retry: 5, queue: :long
 
   def perform(dropbox_file_id)
-    dropbox_file = DropboxFile.find(dropbox_file_id)
-    dropbox_file.transfer_to_s3
+    # The newly created row and its cancellation state may not have replicated yet.
+    ApplicationRecord.connected_to(role: :writing) do
+      dropbox_file = DropboxFile.find(dropbox_file_id)
+      dropbox_file.transfer_to_s3
+    end
   end
 end
