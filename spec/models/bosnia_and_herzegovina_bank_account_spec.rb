539b27fcb63885cf772af65aeba407b98c284c14
gumclaw <gumclaw@antiwork.local>
2026-09-23 10:32:34 -0400

    Trim a spec comment that narrated how the defect was found

--- spec/models/bosnia_and_herzegovina_bank_account_spec.rb
+++ spec/models/bosnia_and_herzegovina_bank_account_spec.rb
@@ -77,7 +77,6 @@ describe BosniaAndHerzegovinaBankAccount do
 
   describe "bank code country" do
     it "rejects a BIC registered outside Bosnia and Herzegovina" do
-      # ZEN.COM's Lithuanian BIC: a foreign BIC here never attaches to Stripe.
       bank_account = build(:bosnia_and_herzegovina_bank_account, bank_number: "BZENLT22")
 
       expect(bank_account).not_to be_valid
