642011d8bccb2eef270098130f2a9c57afa30c76
gumclaw <gumclaw@gumroad.com>
2026-09-24 05:57:09 -0400

    Render a charged preorder's product JSON when its purchase has no url_redirect (#7929)

--- spec/models/preorder_spec.rb
+++ spec/models/preorder_spec.rb
@@ -32,6 +32,25 @@ describe Preorder, :vcr do
       expect(json_hash[:preorder_data]).to eq preorder_data
     end
 
+    it "returns proper json for a charged preorder whose charge purchase has no url redirect" do
+      buyer = create(:user)
+      link = create(:product, price_cents: 600)
+      preorder_link = create(:preorder_link, link:, release_at: 2.days.from_now)
+      authorization_purchase = create(:preorder_authorization_purchase, link:, seller: link.user, purchaser: buyer)
+      preorder = create(:preorder, preorder_link:, seller: link.user, purchaser: buyer, state: "charge_successful")
+      authorization_purchase.update!(preorder:)
+      charge_purchase = create(:purchase, link:, preorder:, seller: link.user, purchaser: buyer)
+
+      json_hash = preorder.mobile_json_data
+
+      expect(json_hash["name"]).to eq link.name
+      expect(json_hash[:purchase_id]).to eq charge_purchase.external_id
+      expect(json_hash[:purchased_at]).to eq charge_purchase.created_at
+      expect(json_hash[:user_id]).to eq buyer.external_id
+      expect(json_hash[:is_archived]).to eq false
+      expect(json_hash[:preorder_data]).to eq nil
+    end
+
     it "returns proper json for a preorder with a url redirect post charge" do
       travel_to(Time.current) do
         good_card = build(:chargeable)
